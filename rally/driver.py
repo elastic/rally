cd06b05be90475db24746b5191034de3287c6a08
Honza Král <honza.kral@gmail.com>
2015-12-09 20:45:21 +0100

    Replace custom bulk logic with elasticsearch-py helpers
    Notable changes:
     * ids are not being materialized, instead are being generated on the
       fly
     * minor cleanup where not as many values are being passed around
     * there might be minor discrepancies in on-line reporting of indexing
       stats since we are counting bytes sent (not necessarilly processed)
       and documents processed.
     * index/type are now specified on request level, not as part of the
       body
     * MB/s now only contains docs, not action lines

--- rally/driver.py
+++ rally/driver.py
@@ -7,6 +7,8 @@ import gzip
 import os
 import logging
 
+from elasticsearch.helpers import parallel_bulk, streaming_bulk
+
 import rally.metrics as m
 import rally.track.track
 import rally.utils.process
@@ -124,6 +126,7 @@ class SearchBenchmark(TimedOperation):
     return d
 
 
+
 class IndexBenchmark(TimedOperation):
   def __init__(self, config, track, track_setup, cluster, metrics):
     self._config = config
@@ -140,34 +143,8 @@ class IndexBenchmark(TimedOperation):
     docs_to_index = track.number_of_documents
     data_set_path = self._config.opts("benchmarks", "dataset.path")
 
-    conflicts = track_setup.test_settings.id_conflicts
-    if conflicts is not rally.track.track.IndexIdConflict.NoConflicts:
-      ids = self.buildIDs(conflicts)
-    else:
-      ids = None
 
     logger.info('Use %d docs per bulk request' % DOCS_IN_BLOCK)
-    self._bulk_docs = BulkDocs(track.index_name,
-                               track.type_name,
-                               data_set_path,
-                               ids,
-                               docs_to_index,
-                               rand,
-                               DOCS_IN_BLOCK)
-
-  def buildIDs(self, conflicts):
-    docs_to_index = self._track.number_of_documents
-    logger.info('build ids with id conflicts of type %s' % conflicts)
-
-    all_ids = [0] * docs_to_index
-    for i in range(docs_to_index):
-      if conflicts == rally.track.track.IndexIdConflict.SequentialConflicts:
-        all_ids[i] = '%10d' % i
-      elif conflicts == rally.track.track.IndexIdConflict.RandomConflicts:
-        all_ids[i] = '%10d' % rand.randint(0, docs_to_index)
-      else:
-        raise RuntimeError('Unknown id conflict type %s' % conflicts)
-    return all_ids
 
   def run(self):
     docs_to_index = self._track.number_of_documents
@@ -183,7 +160,7 @@ class IndexBenchmark(TimedOperation):
 
     try:
       # TODO dm: Reduce number of parameters...
-      self.indexAllDocs(data_set_path, self._track.index_name, self._cluster.client(), self._bulk_docs, expectedDocCount)
+      self.indexAllDocs(data_set_path, expectedDocCount)
       # just for ending the progress output
       print("")
       finished = True
@@ -191,89 +168,99 @@ class IndexBenchmark(TimedOperation):
     finally:
       # HINT dm: Not reporting relevant
       self.print_metrics('Finished?: %s' % finished)
-      self._bulk_docs.close()
 
-  def indexBulkDocs(self, es, startingGun, myID, bulkDocs, failedEvent, stopEvent, pauseSec=None):
 
-    """
-    Runs one (client) bulk index thread.
-    """
+  def generate_ids(self, conflicts):
+    docs_to_index = self._track.number_of_documents
+    logger.info('build ids with id conflicts of type %s' % conflicts)
 
-    startingGun.await()
+    if conflicts == rally.track.track.IndexIdConflict.SequentialConflicts:
+      yield from (
+          '%10d' % (
+              rand.randint(0, i)
+              # pick already returned id in 25% of cases
+              if i > 0 and self.rand.randint(0, 3) == 3
+              else i
+          ) for i in range(docs_to_index)
+      )
+    elif conflicts == rally.track.track.IndexIdConflict.RandomConflicts:
+      ids = []
+      for _ in range(docs_to_index):
+        if ids and selfrand.randint(0, 3) == 3:
+          # pick already returned id in 25%
+          id = rand.choice(ids)
+        else:
+          id = '%10i' % rand.randint(0, docs_to_index)
+          ids.append(id)
+      yield id
+    else:
+      raise RuntimeError('Unknown id conflict type %s' % conflicts)
 
-    while not stopEvent.isSet():
-      # t0 = time.time()
-      buffer = bulkDocs.nextNDocs()
-      if len(buffer) == 0:
-        break
-      # t1 = time.time()
-      # self.print_metrics('IndexerThread%d: get took %.1f msec' % (myID, 1000*(t1-t0)))
+  def get_expand_action(self):
+    conflicts = self._track_setup.test_settings.id_conflicts
+    if conflicts is not rally.track.track.IndexIdConflict.NoConflicts:
+      id_generator = self.generate_ids(conflicts)
+    else:
+      id_generator = None
 
-      count = int(len(buffer) / 2)
-      data = '\n'.join(buffer)
-      del buffer[:]
-
-      try:
-        result = es.bulk(body=data, params={'request_timeout': 60000})
-      except BaseException as e:
-        logger.error("Indexing failed: %s" % e)
-        failedEvent.set()
-        stopEvent.set()
-        raise
+    def expand_action(data):
+      if id_generator:
+        id = next(id_generator)
+        action = '{"index": {"_id": %d}}' % next(id_generator)
       else:
-        if result['errors'] != False or len(result['items']) != count:
-          self.print_metrics('bulk failed (count=%s):' % count)
-          self.print_metrics('%s' % json.dumps(result, sort_keys=True,
-                                               indent=4, separators=(',', ': ')))
-          failedEvent.set()
-          stopEvent.set()
-          raise RuntimeError('bulk failed')
+        action = '{"index": {}}'
+      self._sent_bytes += len(data)
+      return action, data.strip()
+    return expand_action
 
-      # t2 = time.time()
-      # logger.info('IndexerThread%d: index took %.1f msec' % (myID, 1000*(t2-t1)))
-      self.printStatus(count, len(data))
-      if pauseSec is not None:
-        time.sleep(pauseSec)
+  def _open_file(self, docsFile):
+    if docsFile.endswith('.bz2'):
+      return bz2.open(docsFile, 'rt')
+    elif docsFile.endswith('.gz'):
+      return gzip.open(docsFile, 'rt')
+    else:
+      return open(docsFile, 'rt')
 
-  def indexAllDocs(self, docsFile, indexName, es, bulkDocs, expectedDocCount, doFlush=True, doStats=True):
+  def _read_records(self, docsFile):
+    with self._open_file(docsFile) as f:
+      yield from f
+
+  def indexAllDocs(self, docsFile, expectedDocCount, doFlush=True, doStats=True):
     numClientThreads = int(self._config.opts("benchmarks", "index.client.threads"))
-    starting_gun = CountDownLatch(1)
     self.print_metrics('json docs file: %s' % docsFile)
 
-    stopEvent = threading.Event()
-    failedEvent = threading.Event()
+    indexName = self._track.index_name
+    typeName = self._track.type_name
+    es = self._cluster.client()
 
+
+    self.print_metrics('Launching %d client bulk indexing threads' % numClientThreads)
+    self.startTime = time.time()
+    self._sent_bytes = 0
+
+    processed = 0
+    self.printStatus(processed)
     try:
-      # Launch all threads
-      self.print_metrics('Launching %d client bulk indexing threads' % numClientThreads)
-      threads = []
-      for i in range(numClientThreads):
-        t = threading.Thread(target=self.indexBulkDocs, args=(es, starting_gun, i, bulkDocs, failedEvent, stopEvent))
-        t.setDaemon(True)
-        t.start()
-        threads.append(t)
-
-      # Tell all threads to start:
-      starting_gun.countDown()
-      self.startTime = time.time()
-
-      for t in threads:
-        t.join()
-
+      for _ in parallel_bulk(es,
+                        self._read_records(docsFile),
+                        thread_count=numClientThreads,
+                        index=indexName,
+                        doc_type=typeName,
+                        chunk_size=DOCS_IN_BLOCK,
+                        expand_action_callback=self.get_expand_action(),
+                        ):
+        if processed % 10000 == 0:
+          self.printStatus(processed)
+        processed += 1
     except KeyboardInterrupt:
       self.print_metrics('\nSIGINT: now stop')
-      stopEvent.set()
-      for t in threads:
-        t.join()
 
-    if failedEvent.isSet():
-      raise RuntimeError('some indexing threads failed')
+    self.printStatus(processed)
 
     end_time = time.time()
     # HINT dm: Reporting relevant
-    self.print_metrics('Total docs/sec: %.1f' % (bulkDocs.indexedDocCount / (end_time - self.startTime)))
+    self.print_metrics('Total docs/sec: %.1f' % (processed / (end_time - self.startTime)))
 
-    self.printStatus(0, 0)
 
     if doFlush:
       self.print_metrics('now force flush')
@@ -314,24 +301,20 @@ class IndexBenchmark(TimedOperation):
   def print_metrics(self, message):
     self._metrics.collect(message)
 
-  def printStatus(self, incDocs, incBytes):
+  def printStatus(self, docs_processed):
     # TODO dm: Move (somehow) to metrics collector
     # FIXME dm: Well, the method name says it all
     with self._metrics.expose_print_lock_dirty_hack_remove_me_asap():
-      self._numDocsIndexed += incDocs
-      self._totBytesIndexed += incBytes
-      if self._numDocsIndexed >= self._nextPrint or incDocs == 0:
         t = time.time()
-        docs_per_second = self._numDocsIndexed / (t - self.startTime)
-        mb_per_second = rally.utils.format.bytes_to_mb(self._totBytesIndexed) / (t - self.startTime)
+        docs_per_second = docs_processed / (t - self.startTime)
+        mb_per_second = rally.utils.format.bytes_to_mb(self._sent_bytes) / (t - self.startTime)
         self._progress.print(
           "  Benchmarking indexing at %.1f docs/s, %.1f MB/sec" % (docs_per_second, mb_per_second),
           # "docs: %d / %d [%3d%%]" % (self._numDocsIndexed, self._track.number_of_documents, round(100 * self._numDocsIndexed / self._track.number_of_documents))
-          "[%3d%% done]" % round(100 * self._numDocsIndexed / self._track.number_of_documents)
+          "[%3d%% done]" % round(100 * docs_processed / self._track.number_of_documents)
         )
         logger.info(
-          'Indexer: %d docs: %.2f sec [%.1f dps, %.1f MB/sec]' % (self._numDocsIndexed, t - self.startTime, docs_per_second, mb_per_second))
-        self._nextPrint += 10000
+          'Indexer: %d docs: %.2f sec [%.1f dps, %.1f MB/sec]' % (docs_processed, t - self.startTime, docs_per_second, mb_per_second))
 
   def printIndexStats(self, dataDir):
     indexSizeKB = os.popen('du -s %s' % dataDir).readline().split()[0]
@@ -343,95 +326,4 @@ class IndexBenchmark(TimedOperation):
     rally.utils.process.run_subprocess('find %s -ls' % dataDir)
 
 
-class CountDownLatch:
-  def __init__(self, count=1):
-    self.count = count
-    self.lock = threading.Condition()
 
-  def countDown(self):
-    with self.lock:
-      self.count -= 1
-
-      if self.count <= 0:
-        self.lock.notifyAll()
-
-  def await(self):
-    with self.lock:
-      while self.count > 0:
-        self.lock.wait()
-
-
-class BulkDocs:
-  """
-  Pulls docs out of a one-json-line-per-doc file and makes bulk requests.
-  """
-
-  def __init__(self, indexName, typeName, docsFile, ids, docsToIndex, rand, docsInBlock):
-    if docsFile.endswith('.bz2'):
-      self.f = bz2.BZ2File(docsFile)
-    elif docsFile.endswith('.gz'):
-      self.f = gzip.open(docsFile)
-    else:
-      self.f = open(docsFile, 'rb')
-    self.indexName = indexName
-    self.typeName = typeName
-    self.blockCount = 0
-    self.idUpto = 0
-    self.ids = ids
-    self.fileLock = threading.Lock()
-    self.docsToIndex = docsToIndex
-    self.rand = rand
-    self.docsInBlock = docsInBlock
-    self.indexedDocCount = 0
-
-  def close(self):
-    if self.f is None:
-      self.f.close()
-      self.f = None
-
-  def nextNDocs(self):
-
-    """
-    Returns lines for one bulk request.
-    """
-
-    buffer = []
-
-    with self.fileLock:
-      docsLeft = self.docsToIndex - (self.blockCount * self.docsInBlock)
-      if self.f is None or docsLeft <= 0:
-        return []
-
-      self.blockCount += 1
-      limit = 2 * min(self.docsInBlock, docsLeft)
-
-      while True:
-        line = self.f.readline()
-        if len(line) == 0:
-          self.close()
-          break
-        line = line.decode('utf-8')
-        line = line.rstrip()
-
-        if self.ids is not None:
-          # 25% of the time we replace a doc:
-          if self.idUpto > 0 and self.rand.randint(0, 3) == 3:
-            id = self.ids[self.rand.randint(0, self.idUpto - 1)]
-          else:
-            id = self.ids[self.idUpto]
-            self.idUpto += 1
-          # TODO: can't we set default index & type in the bulk request, instead of per doc here?
-          cmd = '{"index": {"_index": "%s", "_type": "%s", "_id": "%s"}}' % (self.indexName, self.typeName, id)
-        else:
-          # TODO: can't we set default index & type in the bulk request, instead of per doc here?
-          cmd = '{"index": {"_index": "%s", "_type": "%s"}}' % (self.indexName, self.typeName)
-
-        buffer.append(cmd)
-        buffer.append(line)
-
-        if len(buffer) >= limit:
-          break
-
-    self.indexedDocCount += len(buffer) / 2
-
-    return buffer
