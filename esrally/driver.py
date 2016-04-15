e22ec3e8374e7b1393738c6e1b3ce5c77889ab68
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-04-15 14:35:04 +0200

    Use classic indexing code instead of parallel_bulk
    Testing has shown that parallel_bulk seems to produce a major
    bottleneck in the index benchmarking code. Hence, we replace
    it again with the indexing driver based on the classic benchmarks.
    
    Relates #9

--- esrally/driver.py
+++ esrally/driver.py
@@ -1,8 +1,7 @@
 import json
 import random
 import logging
-
-from elasticsearch.helpers import parallel_bulk
+import threading
 
 from esrally import time
 from esrally.track import track
@@ -138,10 +137,9 @@ class IndexBenchmark(TimedOperation):
         self._metrics_store = cluster.metrics_store
         self._metrics = metrics
         self._quiet_mode = self._config.opts("system", "quiet.mode")
-        self._sent_bytes = 0
         self._progress = progress.CmdLineProgressReporter()
-        self._rand = random.Random(17)
         self._bulk_size = 5000
+        self.processed = 0
         logger.info("Use %d docs per bulk request" % self._bulk_size)
 
     def run(self):
@@ -153,102 +151,34 @@ class IndexBenchmark(TimedOperation):
             self._progress.finish()
             logger.info("IndexBenchmark finished successfully: %s" % finished)
 
-    def generate_ids(self, conflicts):
-        docs_to_index = self._track.number_of_documents
-        logger.info("build ids with id conflicts of type %s" % conflicts)
-
-        if conflicts == track.IndexIdConflict.SequentialConflicts:
-            yield from (
-                "%10d" % (
-                    self._rand.randint(0, i)
-                    # pick already returned id in 25% of cases
-                    if i > 0 and self._rand.randint(0, 3) == 3
-                    else i
-                ) for i in range(docs_to_index)
-            )
-        elif conflicts == track.IndexIdConflict.RandomConflicts:
-            ids = []
-            for _ in range(docs_to_index):
-                if ids and self._rand.randint(0, 3) == 3:
-                    # pick already returned id in 25%
-                    id = self._rand.choice(ids)
-                else:
-                    id = "%10d" % self._rand.randint(0, docs_to_index)
-                    ids.append(id)
-                yield id
-        else:
-            raise RuntimeError("Unknown id conflict type %s" % conflicts)
-
-    def get_expand_action(self):
-        conflicts = self._track_setup.benchmark_settings.id_conflicts
-        if conflicts is not track.IndexIdConflict.NoConflicts:
-            id_generator = self.generate_ids(conflicts)
-        else:
-            id_generator = None
-
-        def expand_action(data):
-            if id_generator:
-                action = '{"index": {"_id": %s}}' % next(id_generator)
-            else:
-                action = '{"index": {}}'
-            self._sent_bytes += len(data)
-            return action, data.strip()
-
-        return expand_action
-
-    def _read_records(self, documents):
-        logger.info("Indexing JSON docs file: [%s]" % documents)
-        with open(documents, "rt") as f:
-            yield from f
-
     def index(self):
         force_merge = self._track_setup.benchmark_settings.force_merge
         docs_to_index = self._track.number_of_documents
-        documents = self._config.opts("benchmarks", "dataset.path")
 
         # We cannot know how many docs have been updated if we produce id conflicts
-        if self._track_setup.benchmark_settings.id_conflicts == track.IndexIdConflict.NoConflicts:
+        conflicts = self._track_setup.benchmark_settings.id_conflicts
+        if conflicts == track.IndexIdConflict.NoConflicts:
             expected_doc_count = docs_to_index
+            ids = None
         else:
             expected_doc_count = None
-        num_client_threads = int(self._config.opts("benchmarks", "index.client.threads"))
-        logger.info("Launching %d client bulk indexing threads" % num_client_threads)
+            ids = self.build_conflicting_ids(conflicts)
 
-        es = self._cluster.client
-        processed = 0
-        total = self._track.number_of_documents
         self._stop_watch.start()
-        self._print_progress(processed, total)
+        self._print_progress(self.processed)
 
         for index in self._track.indices:
             for type in index.types:
                 if type.document_file_name:
-                    try:
-                        for _ in parallel_bulk(es,
-                                               self._read_records(documents[type]),
-                                               thread_count=num_client_threads,
-                                               index=index.name,
-                                               doc_type=type.name,
-                                               chunk_size=self._bulk_size,
-                                               expand_action_callback=self.get_expand_action(),
-                                               ):
-                            if processed % 10000 == 0:
-                                self._print_progress(processed, total)
-                                # use 10% of all documents for warmup before gathering metrics
-                                if processed > total / 10:
-                                    elapsed = self._stop_watch.split_time()
-                                    docs_per_sec = (processed / elapsed)
-                                    self._metrics_store.put_value_cluster_level("indexing_throughput", round(docs_per_sec), "docs/s")
-                            processed += 1
-                    except KeyboardInterrupt:
-                        logger.info("Received SIGINT: IndexBenchmark will be stopped prematurely.")
+                    self.index_documents(index, type, ids)
 
         self._stop_watch.stop()
-        self._print_progress(processed, total)
-        docs_per_sec = (processed / self._stop_watch.total_time())
+        self._print_progress(self.processed)
+        docs_per_sec = (self.processed / self._stop_watch.total_time())
         self._metrics_store.put_value_cluster_level("indexing_throughput", round(docs_per_sec), "docs/s")
 
         logger.info("Force flushing all indices.")
+        es = self._cluster.client
         es.indices.flush(params={"request_timeout": 600})
         logger.info("Force flush has finished successfully.")
 
@@ -258,6 +188,20 @@ class IndexBenchmark(TimedOperation):
 
         self._index_stats(expected_doc_count)
         self._node_stats()
+
+    def build_conflicting_ids(self, conflicts):
+        docs_to_index = self._track.number_of_documents
+        logger.info('build ids with id conflicts of type %s' % conflicts)
+
+        all_ids = [0] * docs_to_index
+        for i in range(docs_to_index):
+            if conflicts == track.IndexIdConflict.SequentialConflicts:
+                all_ids[i] = '%10d' % i
+            elif conflicts == track.IndexIdConflict.RandomConflicts:
+                all_ids[i] = '%10d' % random.randint(0, docs_to_index)
+            else:
+                raise RuntimeError('Unknown id conflict type %s' % conflicts)
+        return all_ids
 
     def _index_stats(self, expected_doc_count):
         logger.info("Gathering indices stats")
@@ -269,13 +213,18 @@ class IndexBenchmark(TimedOperation):
         primaries = stats["_all"]["primaries"]
         self._metrics_store.put_count_cluster_level("segments_count", primaries["segments"]["count"])
         self._metrics_store.put_count_cluster_level("segments_memory_in_bytes", primaries["segments"]["memory_in_bytes"], "byte")
-        self._metrics_store.put_count_cluster_level("segments_doc_values_memory_in_bytes", primaries["segments"]["doc_values_memory_in_bytes"], "byte")
-        self._metrics_store.put_count_cluster_level("segments_stored_fields_memory_in_bytes", primaries["segments"]["stored_fields_memory_in_bytes"],
-                                      "byte")
-        self._metrics_store.put_count_cluster_level("segments_terms_memory_in_bytes", primaries["segments"]["terms_memory_in_bytes"], "byte")
-        self._metrics_store.put_count_cluster_level("segments_norms_memory_in_bytes", primaries["segments"]["norms_memory_in_bytes"], "byte")
+        self._metrics_store.put_count_cluster_level("segments_doc_values_memory_in_bytes",
+                                                    primaries["segments"]["doc_values_memory_in_bytes"], "byte")
+        self._metrics_store.put_count_cluster_level("segments_stored_fields_memory_in_bytes",
+                                                    primaries["segments"]["stored_fields_memory_in_bytes"],
+                                                    "byte")
+        self._metrics_store.put_count_cluster_level("segments_terms_memory_in_bytes", primaries["segments"]["terms_memory_in_bytes"],
+                                                    "byte")
+        self._metrics_store.put_count_cluster_level("segments_norms_memory_in_bytes", primaries["segments"]["norms_memory_in_bytes"],
+                                                    "byte")
         self._metrics_store.put_value_cluster_level("merges_total_time", primaries["merges"]["total_time_in_millis"], "ms")
-        self._metrics_store.put_value_cluster_level("merges_total_throttled_time", primaries["merges"]["total_throttled_time_in_millis"], "ms")
+        self._metrics_store.put_value_cluster_level("merges_total_throttled_time", primaries["merges"]["total_throttled_time_in_millis"],
+                                                    "ms")
         self._metrics_store.put_value_cluster_level("indexing_total_time", primaries["indexing"]["index_time_in_millis"], "ms")
         self._metrics_store.put_value_cluster_level("refresh_total_time", primaries["refresh"]["total_time_in_millis"], "ms")
         self._metrics_store.put_value_cluster_level("flush_total_time", primaries["flush"]["total_time_in_millis"], "ms")
@@ -319,11 +268,11 @@ class IndexBenchmark(TimedOperation):
             times.append(duration)
         return times, result
 
-    def _print_progress(self, docs_processed, docs_total):
+    def _print_progress(self, docs_processed):
         if not self._quiet_mode:
             # stack-confine asap to keep the reporting error low (this number may be updated by other threads), we don't need to be entirely
             # accurate here as this is "just" a progress report for the user
-            sent = self._sent_bytes
+            docs_total = self._track.number_of_documents
             elapsed = self._stop_watch.split_time()
             docs_per_second = docs_processed / elapsed
             self._progress.print(
@@ -336,3 +285,160 @@ class IndexBenchmark(TimedOperation):
         index_size_bytes = io.get_size(data_dir)
         self._metrics_store.put_count_cluster_level("final_index_size_bytes", index_size_bytes, "byte")
         process.run_subprocess_with_logging("find %s -ls" % data_dir, header="index files:")
+
+    def index_documents(self, index, type, ids):
+        num_client_threads = int(self._config.opts("benchmarks", "index.client.threads"))
+        logger.info("Launching %d client bulk indexing threads" % num_client_threads)
+
+        documents = self._config.opts("benchmarks", "dataset.path")
+        docs_file = documents[type]
+        logger.info("Indexing JSON docs file: [%s]" % docs_file)
+
+        start_signal = CountDownLatch(1)
+        stop_event = threading.Event()
+        failed_event = threading.Event()
+        docs_to_index = self._track.number_of_documents
+
+        bulk_docs = BulkDocs(index.name, type.name, docs_file, ids, docs_to_index, self._bulk_size)
+
+        threads = []
+        try:
+            for i in range(num_client_threads):
+                t = threading.Thread(target=self.bulk_index, args=(start_signal, bulk_docs, failed_event, stop_event))
+                t.setDaemon(True)
+                t.start()
+                threads.append(t)
+
+            start_signal.countDown()
+            for t in threads:
+                t.join()
+
+        except KeyboardInterrupt:
+            stop_event.set()
+            for t in threads:
+                t.join()
+
+        if failed_event.isSet():
+            raise RuntimeError("some indexing threads failed")
+
+    def bulk_index(self, start_signal, bulk_docs, failed_event, stop_event):
+        """
+        Runs one (client) bulk index thread.
+        """
+        start_signal.await()
+
+        while not stop_event.isSet():
+            buffer, document_count = bulk_docs.next_bulk()
+            if document_count == 0:
+                break
+            data = "\n".join(buffer)
+            del buffer[:]
+
+            try:
+                self._cluster.client.bulk(body=data, params={'request_timeout': 60000})
+            except BaseException as e:
+                logger.error("Indexing failed: %s" % e)
+                failed_event.set()
+                stop_event.set()
+                raise
+            self.update_bulk_status(document_count)
+
+    def update_bulk_status(self, count):
+        self.processed += count
+        if self.processed % 10000 == 0:
+
+            self._print_progress(self.processed)
+            # use 10% of all documents for warmup before gathering metrics
+            if self.processed > self._track.number_of_documents / 10:
+                elapsed = self._stop_watch.split_time()
+                docs_per_sec = (self.processed / elapsed)
+                self._metrics_store.put_value_cluster_level("indexing_throughput", round(docs_per_sec), "docs/s")
+
+
+class CountDownLatch:
+    def __init__(self, count=1):
+        self.count = count
+        self.lock = threading.Condition()
+
+    def countDown(self):
+        with self.lock:
+            self.count -= 1
+
+            if self.count <= 0:
+                self.lock.notifyAll()
+
+    def await(self):
+        with self.lock:
+            while self.count > 0:
+                self.lock.wait()
+
+
+class BulkDocs:
+    """
+    Pulls docs out of a one-json-line-per-doc file and makes bulk requests.
+    """
+
+    def __init__(self, index_name, type_name, documents, ids, docs_to_index, bulk_size):
+        self.f = open(documents, 'rt')
+        self.index_name = index_name
+        self.type_name = type_name
+        self.current_bulk = 0
+        self.id_up_to = 0
+        self.ids = ids
+        self.file_lock = threading.Lock()
+        self.docs_to_index = docs_to_index
+        self.rand = random.Random(17)
+        self.bulk_size = bulk_size
+        self.indexed_document_count = 0
+
+    def close(self):
+        if self.f:
+            self.f.close()
+            self.f = None
+
+    def next_bulk(self):
+
+        """
+        Returns lines for one bulk request.
+        """
+
+        buffer = []
+        try:
+            with self.file_lock:
+                docs_left = self.docs_to_index - (self.current_bulk * self.bulk_size)
+                if self.f is None or docs_left <= 0:
+                    return [], 0
+
+                self.current_bulk += 1
+                limit = 2 * min(self.bulk_size, docs_left)
+
+                while True:
+                    line = self.f.readline()
+                    if len(line) == 0:
+                        self.close()
+                        break
+                    line = line.strip()
+
+                    if self.ids is not None:
+                        # 25% of the time we replace a doc:
+                        if self.id_up_to > 0 and self.rand.randint(0, 3) == 3:
+                            id = self.ids[self.rand.randint(0, self.id_up_to - 1)]
+                        else:
+                            id = self.ids[self.id_up_to]
+                            self.id_up_to += 1
+                        cmd = '{"index": {"_index": "%s", "_type": "%s", "_id": "%s"}}' % (self.index_name, self.type_name, id)
+                    else:
+                        cmd = '{"index": {"_index": "%s", "_type": "%s"}}' % (self.index_name, self.type_name)
+
+                    buffer.append(cmd)
+                    buffer.append(line)
+
+                    if len(buffer) >= limit:
+                        break
+
+            # contains index commands and data (in bulk format), hence 2 lines per record
+            document_count = len(buffer) / 2
+            self.indexed_document_count += document_count
+            return buffer, document_count
+        except BaseException as e:
+            logger.exception("Could not read")
