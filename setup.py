b9a052ba0010be02ad00d2f65793ae92f3b594b2
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-06-09 13:36:47 +0200

    Continue 0.3.1 dev

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from os.path import join, dirname
 from setuptools import setup, find_packages
 
-VERSION = (0, 3, 0)
+VERSION = (0, 3, 1, "dev0")
 __version__ = VERSION
 __versionstr__ = ".".join(map(str, VERSION))
 
