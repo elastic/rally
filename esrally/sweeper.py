bbb425fa70104923937c4fd801b8f7f3bd4e2598
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-06-28 10:42:24 +0200

    Add a hint about the compressed log file path

--- esrally/sweeper.py
+++ esrally/sweeper.py
@@ -11,6 +11,8 @@ class Sweeper:
     def __call__(self, track, challenge, car):
         invocation_root = paths.Paths(self._config).invocation_root()
         log_root = paths.Paths(self._config).log_root()
-        io.compress(log_root, "%s/logs-%s-%s-%s.zip" % (invocation_root, track.name, challenge.name, car.name))
+        archive_path = "%s/logs-%s-%s-%s.zip" % (invocation_root, track.name, challenge.name, car.name)
+        io.compress(log_root, archive_path)
+        print("\nLogs for this race are archived in %s" % archive_path)
         shutil.rmtree(log_root)
 
