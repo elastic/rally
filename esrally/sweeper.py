83363fd93ecf783125638fd9f3a68320b466cede
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-07-01 13:52:20 +0200

    Correct log archive name for external benchmarks

--- esrally/sweeper.py
+++ esrally/sweeper.py
@@ -11,7 +11,12 @@ class Sweeper:
     def __call__(self, track, challenge, car):
         invocation_root = paths.Paths(self._config).invocation_root()
         log_root = paths.Paths(self._config).log_root()
-        archive_path = "%s/logs-%s-%s-%s.zip" % (invocation_root, track.name, challenge.name, car.name)
+        # for external benchmarks, there is no match to a car
+        if car:
+            car_suffix = "-%s" % car.name
+        else:
+            car_suffix = ""
+        archive_path = "%s/logs-%s-%s%s.zip" % (invocation_root, track.name, challenge.name, car_suffix)
         io.compress(log_root, archive_path)
         print("\nLogs for this race are archived in %s" % archive_path)
         shutil.rmtree(log_root)
