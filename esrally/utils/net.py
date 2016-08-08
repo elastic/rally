e5e4e8bfe4b26cae3f50e8e66caf964c41cc21ad
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-08-08 08:38:58 +0200

    Improve robustness of HTTP downloads

--- esrally/utils/net.py
+++ esrally/utils/net.py
@@ -2,6 +2,8 @@ import os
 import shutil
 import urllib.request
 
+import urllib3
+
 from esrally import exceptions
 from esrally.utils import process
 
@@ -10,7 +12,8 @@ def download(url, local_path, expected_size_in_bytes=None):
     """
     Downloads a single file from a URL to the provided local path.
 
-    :param url: The remote URL specifying one file that should be downloaded. May be either a HTTP or HTTPS URL. If s3cmd is set up correctly on the system, S3 URL are also supported.
+    :param url: The remote URL specifying one file that should be downloaded. May be either a HTTP or HTTPS URL. If s3cmd is set up
+    correctly on the system, S3 URL are also supported.
     :param local_path: The local file name of the file that should be downloaded.
     :param expected_size_in_bytes: The expected file size in bytes if known. It will be used to verify that all data have been downloaded.
     """
@@ -31,10 +34,10 @@ def retrieve_content_as_string(url):
 
 def download_via_http(url, local_path, expected_size_in_bytes=None):
     tmp_data_set_path = local_path + ".tmp"
+    http = urllib3.PoolManager()
     try:
-        urllib.request.urlretrieve(url, tmp_data_set_path)
-        # with urllib.request.urlopen(url) as response, open(tmp_data_set_path, "wb") as out_file:
-        #     shutil.copyfileobj(response, out_file)
+        with http.request("GET", url, preload_content=False, timeout=60) as r, open(tmp_data_set_path, "wb") as out_file:
+            shutil.copyfileobj(r, out_file)
     except:
         if os.path.isfile(tmp_data_set_path):
             os.remove(tmp_data_set_path)
@@ -45,7 +48,7 @@ def download_via_http(url, local_path, expected_size_in_bytes=None):
             if os.path.isfile(tmp_data_set_path):
                 os.remove(tmp_data_set_path)
             raise exceptions.DataError("Download of [%s] is corrupt. Downloaded [%d] bytes but [%d] bytes are expected. Please retry." %
-                               (local_path, download_size, expected_size_in_bytes))
+                                       (local_path, download_size, expected_size_in_bytes))
         os.rename(tmp_data_set_path, local_path)
 
 
@@ -59,8 +62,7 @@ def download_via_s3(url, data_set_path, size_in_bytes):
             # cleanup probably corrupt data file...
             if os.path.isfile(tmp_data_set_path):
                 os.remove(tmp_data_set_path)
-            raise exceptions.SystemSetupError(
-                    "Could not get benchmark data from S3: '%s'. Is s3cmd installed and set up properly?" % s3cmd)
+            raise exceptions.SystemSetupError("Could not get benchmark data from S3: '%s'. Is s3cmd installed and set up properly?" % s3cmd)
     except:
         os.remove(tmp_data_set_path)
         raise
