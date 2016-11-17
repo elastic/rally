9e0b2aabeb67fe1f70febfc5ffc001e4b15a00ab
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2016-11-17 08:21:57 +0100

    Make conversions None-safe

--- esrally/utils/convert.py
+++ esrally/utils/convert.py
@@ -1,32 +1,35 @@
 def bytes_to_kb(b):
-    return b / 1024.0
+    return b / 1024.0 if b else b
 
 
 def bytes_to_mb(b):
-    return b / 1024.0 / 1024.0
+    return b / 1024.0 / 1024.0 if b else b
 
 
 def bytes_to_gb(b):
-    return b / 1024.0 / 1024.0 / 1024.0
+    return b / 1024.0 / 1024.0 / 1024.0 if b else b
 
 
 def seconds_to_ms(s):
-    return s * 1000
+    return s * 1000 if s else s
 
 
 def seconds_to_hour_minute_seconds(s):
-    hours = s // 3600
-    minutes = (s - 3600 * hours) // 60
-    seconds = s - 3600 * hours - 60 * minutes
-    return hours, minutes, seconds
+    if s:
+        hours = s // 3600
+        minutes = (s - 3600 * hours) // 60
+        seconds = s - 3600 * hours - 60 * minutes
+        return hours, minutes, seconds
+    else:
+        return s, s, s
 
 
 def ms_to_seconds(ms):
-    return ms / 1000.0
+    return ms / 1000.0 if ms else ms
 
 
 def ms_to_minutes(ms):
-    return ms / 1000.0 / 60.0
+    return ms / 1000.0 / 60.0 if ms else ms
 
 
 def to_bool(value):
