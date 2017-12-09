def91c2bdf0b13578b2bb53a3d6b462121a4ab8a
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-12-09 17:27:50 +0100

    Call correct binary in virtualenv

--- run.sh
+++ run.sh
@@ -16,6 +16,9 @@ SELF_UPDATE=YES
 # Assume that the "main remote" is called "origin"
 REMOTE="origin"
 
+# While we could also check via the presence of `VIRTUAL_ENV` this is a bit more reliable.
+python3 -c 'import sys; print(sys.real_prefix)' >/dev/null 2>&1 && IN_VIRTUALENV=1 || IN_VIRTUALENV=0
+
 # Check for parameters that are intended for this script. Note that they only work if they're specified at the beginning (due to how
 # the shell builtin `shift` works. We could make it work for arbitrary positions but that's not worth the complexity for such an
 # edge case).
@@ -58,8 +61,6 @@ then
         echo "Auto-updating Rally from ${REMOTE}"
         git rebase ${REMOTE}/master --quiet
         # if we're running in a virtualenv then the --user prefix is not defined (and makes no sense)
-        # While we could also check via the presence of `VIRTUAL_ENV` this is a bit more reliable.
-        python3 -c 'import sys; print(sys.real_prefix)' 2>/dev/null && IN_VIRTUALENV=1 || IN_VIRTUALENV=0
         if [ ${IN_VIRTUALENV} == 0 ]
         then
             python3 setup.py -q develop --user
@@ -84,11 +85,16 @@ export THESPLOG_FILE_MAXSIZE=${THESPLOG_FILE_MAXSIZE:-204800}
 
 # Provide a consistent binary name to the user and hide the fact that we call another binary under the hood.
 export RALLY_ALTERNATIVE_BINARY_NAME=$(basename "$0")
-RALLY_ROOT=$(python3 -c "import site; print(site.USER_BASE)")
-RALLY_BIN=${RALLY_ROOT}/bin/${BINARY_NAME}
-if [ -x "$RALLY_BIN" ]
+if [ ${IN_VIRTUALENV} == 0 ]
 then
-    ${RALLY_BIN} "$@"
+    RALLY_ROOT=$(python3 -c "import site; print(site.USER_BASE)")
+    RALLY_BIN=${RALLY_ROOT}/bin/${BINARY_NAME}
+    if [ -x "$RALLY_BIN" ]
+    then
+        ${RALLY_BIN} "$@"
+    else
+        echo "Cannot execute ${HUMAN_NAME} in ${RALLY_BIN}."
+    fi
 else
-    echo "Cannot execute ${HUMAN_NAME} in ${RALLY_BIN}."
+    ${BINARY_NAME} "$@"
 fi
