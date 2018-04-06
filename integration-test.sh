24a59d7ff8c4aa96cec4e529c897b3215c226dfa
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-04-06 11:50:01 +0200

    Temporarily disable integration tests for older versions
    We disable integration tests for older versions of ES temporarily as we
    need to backport changes from elastic/rally-tracks#41 first.

--- integration-test.sh
+++ integration-test.sh
@@ -9,7 +9,8 @@ readonly CONFIGURATIONS=(integration-test es-integration-test)
 #
 #       java.lang.UnsupportedOperationException: Boot class path mechanism is not supported
 #
-readonly DISTRIBUTIONS=(2.4.6 5.6.7)
+#readonly DISTRIBUTIONS=(2.4.6 5.6.7)
+readonly DISTRIBUTIONS=(6.2.3)
 readonly TRACKS=(geonames nyc_taxis http_logs nested)
 
 readonly ES_METRICS_STORE_VERSION="6.2.1"
