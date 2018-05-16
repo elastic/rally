040634296ce47e6e728e722657a271d6417de771
Dimitrios Liappis <dimitrios.liappis@elastic.co>
2018-05-16 19:29:35 +0300

    Commit ccr-stats telemetry device and tests

--- esrally/mechanic/launcher.py
+++ esrally/mechanic/launcher.py
@@ -79,7 +79,8 @@ class ClusterLauncher:
             telemetry.ClusterEnvironmentInfo(es_default, self.metrics_store),
             telemetry.GcTimesSummary(es_default, self.metrics_store),
             telemetry.IndexStats(es_default, self.metrics_store),
-            telemetry.MlBucketProcessingTime(es_default, self.metrics_store)
+            telemetry.MlBucketProcessingTime(es_default, self.metrics_store),
+            telemetry.CCRStats(telemetry_params, es, self.metrics_store)
         ])
 
         # The list of nodes will be populated by ClusterMetaDataInfo, so no need to do it here
