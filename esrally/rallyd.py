d6626f973b53c9ca9c1a10801da62797899320ee
Daniel Mitterdorfer <daniel.mitterdorfer@gmail.com>
2018-09-10 13:03:29 +0200

    Disable automatic log rotation
    With this commit we disable automatic log rotation in Rally's
    out-of-the-box logging configuration because it can lead to race
    conditions (multiple processes attempting to rollover the log file).
    
    We also migrate existing configurations if possible (i.e. the user did
    not modify it) and provide an example configuration for `logrotate`.
    
    Closes #539
    Relates #560

--- esrally/rallyd.py
+++ esrally/rallyd.py
@@ -52,6 +52,7 @@ def status():
 
 def main():
     check_python_version()
+    log.remove_obsolete_default_log_config()
     log.install_default_log_config()
     log.configure_logging()
     console.init()
