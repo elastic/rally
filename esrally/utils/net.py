d1f45a4c008412d17c7c3faf5e7a88c5bc0f4b2c
Daniel Mitterdorfer <daniel.mitterdorfer@gmail.com>
2019-02-26 11:43:33 +0100

    Properly authenticate at proxy server
    With this commit Rally will set the proper authentication header for
    proxies that require it.
    
    Closes #636
    Relates #652

--- esrally/utils/net.py
+++ esrally/utils/net.py
@@ -31,10 +31,16 @@ def init():
     global __HTTP
     proxy_url = os.getenv("http_proxy")
     if proxy_url and len(proxy_url) > 0:
-        logger.info("Rally connects via proxy URL [%s] to the Internet (picked up from the environment variable [http_proxy]).",  proxy_url)
-        __HTTP = urllib3.ProxyManager(proxy_url, cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+        parsed_url = urllib3.util.parse_url(proxy_url)
+        logger.info("Connecting via proxy URL [%s] to the Internet (picked up from the env variable [http_proxy]).",
+                    proxy_url)
+        __HTTP = urllib3.ProxyManager(proxy_url,
+                                      cert_reqs='CERT_REQUIRED',
+                                      ca_certs=certifi.where(),
+                                      # appropriate headers will only be set if there is auth info
+                                      proxy_headers=urllib3.make_headers(proxy_basic_auth=parsed_url.auth))
     else:
-        logger.info("Rally connects directly to the Internet (no proxy support).")
+        logger.info("Connecting directly to the Internet (no proxy support).")
         __HTTP = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
 
 
