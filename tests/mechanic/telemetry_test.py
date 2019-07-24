809f0d8ad7be9cb6f80251b0a86d6bf122da20ea
Dennis Lawler <dennis.lawler@elastic.co>
2019-07-23 20:55:52 -0700

    Fix unit test breakage cause by changes to JFR telemetry.

--- tests/mechanic/telemetry_test.py
+++ tests/mechanic/telemetry_test.py
@@ -210,7 +210,8 @@ class TransportClient:
 
 class JfrTests(TestCase):
     def test_sets_options_for_pre_java_9_default_recording_template(self):
-        jfr = telemetry.FlightRecorder(telemetry_params={}, log_root="/var/log", java_major_version=random.randint(0, 8))
+        jfr = telemetry.FlightRecorder(telemetry_params={"log_root": "/var/log",
+                                                         "java_major_version": random.randint(0, 8)})
         java_opts = jfr.java_opts("/var/log/test-recording.jfr")
         self.assertEqual(
             ["-XX:+UnlockDiagnosticVMOptions", "-XX:+DebugNonSafepoints", "-XX:+UnlockCommercialFeatures", "-XX:+FlightRecorder",
@@ -218,23 +219,25 @@ class JfrTests(TestCase):
              "dumponexitpath=/var/log/test-recording.jfr", "-XX:StartFlightRecording=defaultrecording=true"], java_opts)
 
     def test_sets_options_for_java_9_or_10_default_recording_template(self):
-        jfr = telemetry.FlightRecorder(telemetry_params={}, log_root="/var/log", java_major_version=random.randint(9, 10))
+        jfr = telemetry.FlightRecorder(telemetry_params={"log_root": "/var/log",
+                                                         "java_major_version": random.randint(9, 10)})
         java_opts = jfr.java_opts("/var/log/test-recording.jfr")
         self.assertEqual(["-XX:+UnlockDiagnosticVMOptions", "-XX:+DebugNonSafepoints", "-XX:+UnlockCommercialFeatures",
                           "-XX:StartFlightRecording=maxsize=0,maxage=0s,disk=true,"
                           "dumponexit=true,filename=/var/log/test-recording.jfr"], java_opts)
 
     def test_sets_options_for_java_11_or_above_default_recording_template(self):
-        jfr = telemetry.FlightRecorder(telemetry_params={}, log_root="/var/log", java_major_version=random.randint(11, 999))
+        jfr = telemetry.FlightRecorder(telemetry_params={"log_root": "/var/log",
+                                                         "java_major_version": random.randint(11, 999)})
         java_opts = jfr.java_opts("/var/log/test-recording.jfr")
         self.assertEqual(["-XX:+UnlockDiagnosticVMOptions", "-XX:+DebugNonSafepoints",
                           "-XX:StartFlightRecording=maxsize=0,maxage=0s,disk=true,"
                           "dumponexit=true,filename=/var/log/test-recording.jfr"], java_opts)
 
     def test_sets_options_for_pre_java_9_custom_recording_template(self):
-        jfr = telemetry.FlightRecorder(telemetry_params={"recording-template": "profile"},
-                                       log_root="/var/log",
-                                       java_major_version=random.randint(0, 8))
+        jfr = telemetry.FlightRecorder(telemetry_params={"recording-template": "profile",
+                                                         "log_root": "/var/log",
+                                                         "java_major_version": random.randint(0, 8)})
         java_opts = jfr.java_opts("/var/log/test-recording.jfr")
         self.assertEqual(
             ["-XX:+UnlockDiagnosticVMOptions", "-XX:+DebugNonSafepoints", "-XX:+UnlockCommercialFeatures", "-XX:+FlightRecorder",
@@ -242,18 +245,18 @@ class JfrTests(TestCase):
              "dumponexitpath=/var/log/test-recording.jfr", "-XX:StartFlightRecording=defaultrecording=true,settings=profile"], java_opts)
 
     def test_sets_options_for_java_9_or_10_custom_recording_template(self):
-        jfr = telemetry.FlightRecorder(telemetry_params={"recording-template": "profile"},
-                                       log_root="/var/log",
-                                       java_major_version=random.randint(9, 10))
+        jfr = telemetry.FlightRecorder(telemetry_params={"recording-template": "profile",
+                                                         "log_root": "/var/log",
+                                                         "java_major_version": random.randint(9, 10)})
         java_opts = jfr.java_opts("/var/log/test-recording.jfr")
         self.assertEqual(["-XX:+UnlockDiagnosticVMOptions", "-XX:+DebugNonSafepoints", "-XX:+UnlockCommercialFeatures",
                           "-XX:StartFlightRecording=maxsize=0,maxage=0s,disk=true,dumponexit=true,"
                           "filename=/var/log/test-recording.jfr,settings=profile"], java_opts)
 
     def test_sets_options_for_java_11_or_above_custom_recording_template(self):
-        jfr = telemetry.FlightRecorder(telemetry_params={"recording-template": "profile"},
-                                       log_root="/var/log",
-                                       java_major_version=random.randint(11, 999))
+        jfr = telemetry.FlightRecorder(telemetry_params={"recording-template": "profile",
+                                                         "log_root": "/var/log",
+                                                         "java_major_version": random.randint(11, 999)})
         java_opts = jfr.java_opts("/var/log/test-recording.jfr")
         self.assertEqual(["-XX:+UnlockDiagnosticVMOptions", "-XX:+DebugNonSafepoints",
                           "-XX:StartFlightRecording=maxsize=0,maxage=0s,disk=true,dumponexit=true,"
