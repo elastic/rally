e7671ac84211b4c3e199363c1a6cc37051368f67
Dennis Lawler <dennis.lawler@elastic.co>
2019-12-16 15:29:00 -0800

    Fix R1702 / too many nested blocks.

--- esrally/track/loader.py
+++ esrally/track/loader.py
@@ -621,6 +621,9 @@ def render_template_from_file(template_file_name, template_vars, complete_track_
 
 
 def filter_included_tasks(t, filters):
+    if not filters:
+        return t
+
     logger = logging.getLogger(__name__)
 
     def match(task, filters):
@@ -629,29 +632,26 @@ def filter_included_tasks(t, filters):
                 return True
         return False
 
-    if not filters:
-        return t
-    else:
-        # always include administrative tasks
-        complete_filters = [track.AdminTaskFilter()] + filters
+    # always include administrative tasks
+    complete_filters = [track.AdminTaskFilter()] + filters
 
-        for challenge in t.challenges:
-            # don't modify the schedule while iterating over it
-            tasks_to_remove = []
-            for task in challenge.schedule:
-                if not match(task, complete_filters):
-                    tasks_to_remove.append(task)
-                else:
-                    leafs_to_remove = []
-                    for leaf_task in task:
-                        if not match(leaf_task, complete_filters):
-                            leafs_to_remove.append(leaf_task)
-                    for leaf_task in leafs_to_remove:
-                        logger.info("Removing sub-task [%s] from challenge [%s] due to task filter.", leaf_task, challenge)
-                        task.remove_task(leaf_task)
-            for task in tasks_to_remove:
-                logger.info("Removing task [%s] from challenge [%s] due to task filter.", task, challenge)
-                challenge.remove_task(task)
+    for challenge in t.challenges:
+        # don't modify the schedule while iterating over it
+        tasks_to_remove = []
+        for task in challenge.schedule:
+            if not match(task, complete_filters):
+                tasks_to_remove.append(task)
+            else:
+                leafs_to_remove = []
+                for leaf_task in task:
+                    if not match(leaf_task, complete_filters):
+                        leafs_to_remove.append(leaf_task)
+                for leaf_task in leafs_to_remove:
+                    logger.info("Removing sub-task [%s] from challenge [%s] due to task filter.", leaf_task, challenge)
+                    task.remove_task(leaf_task)
+        for task in tasks_to_remove:
+            logger.info("Removing task [%s] from challenge [%s] due to task filter.", task, challenge)
+            challenge.remove_task(task)
 
     return t
 
