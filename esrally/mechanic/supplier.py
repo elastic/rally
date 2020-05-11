f7c61bc243a6a96d1eb1bc51ca90146b23734553
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2020-05-11 14:25:52 +0200

    Tighter catch

--- esrally/mechanic/supplier.py
+++ esrally/mechanic/supplier.py
@@ -276,12 +276,11 @@ class CachedElasticsearchSourceSupplier:
             # this can be None if the Elasticsearch does not reside in a git repo and the user has only
             # copied all source files. In that case, we cannot resolve a revision hash and thus we cannot cache.
             if self.cached_path:
-                # noinspection PyBroadException
                 try:
                     shutil.copy(original_path, self.cached_path)
                     self.logger.info("Caching artifact in [%s]", self.cached_path)
                     binaries["elasticsearch"] = self.cached_path
-                except BaseException:
+                except OSError:
                     self.logger.exception("Not caching [%s].", original_path)
             else:
                 self.logger.info("Not caching [%s] (no revision info).", original_path)
