7d56ba3c84aab87d650bf2aeeb8336795ec219b0
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2020-06-15 14:01:12 +0200

    Check for recovery details when finished
    With this commit we defer checking any details about indices recovery
    until recovery has finished. This avoids any issues with properties that
    are not yet returned by the API in earlier stages (e.g.
    `stop_time_in_millis`).

--- esrally/driver/runner.py
+++ esrally/driver/runner.py
@@ -1528,11 +1528,13 @@ class IndicesRecovery(Runner):
                 for _, idx_data in response.items():
                     for _, shard_data in idx_data.items():
                         for shard in shard_data:
-                            all_shards_done = all_shards_done and (shard["stage"] == "DONE")
-                            total_start_millis = min(total_start_millis, shard["start_time_in_millis"])
-                            total_end_millis = max(total_end_millis, shard["stop_time_in_millis"])
-                            idx_size = shard["index"]["size"]
-                            total_recovered += idx_size["recovered_in_bytes"]
+                            current_shard_done = shard["stage"] == "DONE"
+                            all_shards_done = all_shards_done and current_shard_done
+                            if current_shard_done:
+                                total_start_millis = min(total_start_millis, shard["start_time_in_millis"])
+                                total_end_millis = max(total_end_millis, shard["stop_time_in_millis"])
+                                idx_size = shard["index"]["size"]
+                                total_recovered += idx_size["recovered_in_bytes"]
                 self.logger.debug("All shards done for [%s]: [%s].", index, all_shards_done)
 
             if not all_shards_done:
