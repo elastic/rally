66db69ced9245cac370f3c41bb929715f1979fd3
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2020-10-16 21:33:05 +0300

    Add support for Google Cloud Storage buckets
    This commit adds support for GCS hosted buckets storing source data.

--- esrally/utils/net.py
+++ esrally/utils/net.py
@@ -19,6 +19,8 @@ import logging
 import os
 import urllib.error
 
+from urllib.parse import quote
+
 import certifi
 import urllib3
 
@@ -69,7 +71,6 @@ class Progress:
         self.p.finish()
 
 
-# This function is not meant to be called externally and only exists for unit tests
 def _download_from_s3_bucket(bucket_name, bucket_path, local_path, expected_size_in_bytes=None, progress_indicator=None):
     # lazily initialize S3 support - we might not need it
     import boto3.s3.transfer
@@ -94,19 +95,17 @@ def _download_from_s3_bucket(bucket_name, bucket_path, local_path, expected_size
                          Config=boto3.s3.transfer.TransferConfig(use_threads=False))
 
 
-def download_s3(url, local_path, expected_size_in_bytes=None, progress_indicator=None):
-    logger = logging.getLogger(__name__)
+def _build_gcs_object_url(bucket_name, bucket_path):
+    # / and other special characters must be urlencoded in bucket and object names
+    # ref: https://cloud.google.com/storage/docs/request-endpoints#encoding
 
-    bucket_and_path = url[5:]  # s3:// or gs:// prefix
-    bucket_end_index = bucket_and_path.find("/")
-    bucket = bucket_and_path[:bucket_end_index]
-    # we need to remove the leading "/"
-    bucket_path = bucket_and_path[bucket_end_index + 1:]
-
-    logger.info("Downloading from S3 bucket [%s] and path [%s] to [%s].", bucket, bucket_path, local_path)
-    _download_from_s3_bucket(bucket, bucket_path, local_path, expected_size_in_bytes, progress_indicator)
-
-    return expected_size_in_bytes
+    return functools.reduce(urllib.parse.urljoin, [
+        "https://storage.googleapis.com/storage/v1/b/",
+        f"{quote(bucket_name.strip('/'), safe='')}/",
+        "o/",
+        f"{quote(bucket_path.strip('/'), safe='')}",
+        "?alt=media"
+    ])
 
 
 def _download_from_gcs_bucket(bucket_name, bucket_path, local_path, expected_size_in_bytes=None, progress_indicator=None):
@@ -130,15 +129,7 @@ def _download_from_gcs_bucket(bucket_name, bucket_path, local_path, expected_siz
     chunk_size = 50 * 1024 * 1024  # 50MB
 
     with open(local_path, "wb") as local_fp:
-        # / must be urlencoded: https://cloud.google.com/storage/docs/request-endpoints#encoding
-        # media_url = f"https://storage.googleapis.com/storage/v1/b/{bucket_name}/o/{urllib.parse.quote(bucket_path, safe='')}?alt=media"
-        media_url = functools.reduce(urllib.parse.urljoin, [
-            "https://storage.googleapis.com/storage/v1/b/",
-            f"{bucket_name}/",
-            "o/",
-            f"{urllib.parse.quote(bucket_path, safe='')}/",
-            "?alt=media"
-            ])
+        media_url = _build_gcs_object_url(bucket_name, bucket_path)
         download = ChunkedDownload(media_url, chunk_size, local_fp)
         # allow us to calculate the total bytes
         download.consume_next_chunk(transport)
@@ -150,17 +141,18 @@ def _download_from_gcs_bucket(bucket_name, bucket_path, local_path, expected_siz
             download.consume_next_chunk(transport)
 
 
-def download_gcs(url, local_path, expected_size_in_bytes=None, progress_indicator=None):
+def download_from_bucket(blobstore, url, local_path, expected_size_in_bytes=None, progress_indicator=None):
+    blob_downloader = {"s3": _download_from_s3_bucket, "gs": _download_from_gcs_bucket}
     logger = logging.getLogger(__name__)
 
-    bucket_and_path = url[5:]  # gs:// prefix
+    bucket_and_path = url[5:]  # s3:// or gs:// prefix for now
     bucket_end_index = bucket_and_path.find("/")
     bucket = bucket_and_path[:bucket_end_index]
     # we need to remove the leading "/"
     bucket_path = bucket_and_path[bucket_end_index + 1:]
 
-    logger.info("Downloading from GCS bucket [%s] and path [%s] to [%s].", bucket, bucket_path, local_path)
-    _download_from_gcs_bucket(bucket, bucket_path, local_path, expected_size_in_bytes, progress_indicator)
+    logger.info("Downloading from [%s] bucket [%s] and path [%s] to [%s].", blobstore, bucket, bucket_path, local_path)
+    blob_downloader[blobstore](bucket, bucket_path, local_path, expected_size_in_bytes, progress_indicator)
 
     return expected_size_in_bytes
 
@@ -202,10 +194,9 @@ def download(url, local_path, expected_size_in_bytes=None, progress_indicator=No
     """
     tmp_data_set_path = local_path + ".tmp"
     try:
-        if url.startswith("s3"):
-            expected_size_in_bytes = download_s3(url, tmp_data_set_path, expected_size_in_bytes, progress_indicator)
-        elif url.startswith("gs://"):
-            expected_size_in_bytes = download_gcs(url, tmp_data_set_path, expected_size_in_bytes, progress_indicator)
+        scheme = urllib3.util.parse_url(url).scheme
+        if scheme in ["s3", "gs"]:
+            expected_size_in_bytes = download_from_bucket(scheme, url, tmp_data_set_path, expected_size_in_bytes, progress_indicator)
         else:
             expected_size_in_bytes = download_http(url, tmp_data_set_path, expected_size_in_bytes, progress_indicator)
     except BaseException:
