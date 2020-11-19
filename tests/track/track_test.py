elastic/rally

commit 6ed813d69b13a4952b576b285886ce70b149530b

Execute complex request hierarchies (#1112)
With this commit we add the ability to Rally to execute arbitrarily complex
request hierarchies in benchmarks. This is implemented with a new `composite`
operation type. It allows to nest multiple so-called streams which are executed
concurrently. At the moment, the request hierarchy needs to be specified inline
in the track. It is recommended to store more complex request hierarchies in a
separate file and include it with Jinja's `include` directive.

Apart from this change, we also align the internal mapping of operation-type
names to `OperationType` enumeration constants. This is mainly done to simplify
usage and implementation of the `Composite` runner as it can directly use the
operation-types that are specified in the track rather than converting them
internally.

esrally/track/loader.py
@@ -1382,13 +1382,11 @@ class TrackSpecificationReader:
             op = track.OperationType.from_hyphenated_string(op_type_name)
             if "include-in-reporting" not in params:
                 params["include-in-reporting"] = not op.admin_op
-            op_type = op.name
-            self.logger.debug("Using built-in operation type [%s] for operation [%s].", op_type, op_name)
+            self.logger.debug("Using built-in operation type [%s] for operation [%s].", op_type_name, op_name)
         except KeyError:
             self.logger.info("Using user-provided operation type [%s] for operation [%s].", op_type_name, op_name)
-            op_type = op_type_name
 
         try:
-            return track.Operation(name=op_name, meta_data=meta_data, operation_type=op_type, params=params, param_source=param_source)
+            return track.Operation(name=op_name, meta_data=meta_data, operation_type=op_type_name, params=params, param_source=param_source)
         except exceptions.InvalidSyntax as e:
             raise TrackSyntaxError("Invalid operation [%s]: %s" % (op_name, str(e)))

tests/track/loader_test.py
@@ -2888,9 +2888,10 @@ class TrackSpecificationReaderTests(TestCase):
         reader = loader.TrackSpecificationReader()
         resulting_track = reader("unittest", track_specification, "/mappings")
 
-        self.assertEqual(2, len(resulting_track.challenges[0].schedule))
-        self.assertEqual(track.OperationType.Bulk.name, resulting_track.challenges[0].schedule[0].operation.type)
-        self.assertEqual(track.OperationType.ForceMerge.name, resulting_track.challenges[0].schedule[1].operation.type)
+        challenge = resulting_track.challenges[0]
+        self.assertEqual(2, len(challenge.schedule))
+        self.assertEqual(track.OperationType.Bulk.to_hyphenated_string(), challenge.schedule[0].operation.type)
+        self.assertEqual(track.OperationType.ForceMerge.to_hyphenated_string(), challenge.schedule[1].operation.type)
 
     def test_supports_target_throughput(self):
         track_specification = {

tests/track/track_test.py
@@ -225,6 +225,12 @@ class DocumentCorpusTests(TestCase):
                          "Corpora meta-data differ: [{'with-metadata': False}] and [{'with-metadata': True}].")
 
 
+class OperationTypeTests(TestCase):
+    def test_string_hyphenation_is_symmetric(self):
+        for op_type in track.OperationType:
+            self.assertEqual(op_type, track.OperationType.from_hyphenated_string(op_type.to_hyphenated_string()))
+
+
 class TaskTests(TestCase):
     def task(self, schedule=None, target_throughput=None, target_interval=None):
         op = track.Operation("bulk-index", track.OperationType.Bulk.name)

docs/nested-streams.puml
@@ -1,0 +1,32 @@
+@startuml
+' Render this diagram with e.g. http://plantuml.com/plantuml
+concise "Stream 0" as s0
+concise "Stream 0.0" as s00
+concise "Stream 0.1" as s01
+concise "Stream 1" as s1
+
+@0
+s0 is "find-matching-posts"
+s00 is {hidden}
+s01 is {hidden}
+s1 is "load-user"
+
+@+30
+s1 is {-}
+
+@+20
+s0 is "client-processing"
+
+
+@+40
+s0 is {-}
+s00 is "find-related-posts"
+s01 is "find-top-posts"
+
+
+@+40
+s01 is {-}
+
+@+20
+s00 is {-}
+@enduml

esrally/chart_generator.py
@@ -1582,7 +1582,7 @@ class RaceConfig:
         task_names = []
         for task in self.track.find_challenge_or_default(self.challenge).schedule:
             for sub_task in task:
-                if sub_task.operation.type == track.OperationType.Bulk.name:
+                if sub_task.operation.type == track.OperationType.Bulk.to_hyphenated_string():
                     if sub_task.name not in self.excluded_tasks:
                         task_names.append(sub_task.name)
         return task_names

esrally/driver/runner.py
@@ -32,49 +32,50 @@ __RUNNERS = {}
 
 
 def register_default_runners():
-    register_runner(track.OperationType.Bulk.name, BulkIndex(), async_runner=True)
-    register_runner(track.OperationType.ForceMerge.name, ForceMerge(), async_runner=True)
-    register_runner(track.OperationType.IndicesStats.name, Retry(IndicesStats()), async_runner=True)
-    register_runner(track.OperationType.NodesStats.name, NodeStats(), async_runner=True)
-    register_runner(track.OperationType.Search.name, Query(), async_runner=True)
-    register_runner(track.OperationType.RawRequest.name, RawRequest(), async_runner=True)
+    register_runner(track.OperationType.Bulk, BulkIndex(), async_runner=True)
+    register_runner(track.OperationType.ForceMerge, ForceMerge(), async_runner=True)
+    register_runner(track.OperationType.IndexStats, Retry(IndicesStats()), async_runner=True)
+    register_runner(track.OperationType.NodeStats, NodeStats(), async_runner=True)
+    register_runner(track.OperationType.Search, Query(), async_runner=True)
+    register_runner(track.OperationType.RawRequest, RawRequest(), async_runner=True)
+    register_runner(track.OperationType.Composite, Composite(), async_runner=True)
     # This is an administrative operation but there is no need for a retry here as we don't issue a request
-    register_runner(track.OperationType.Sleep.name, Sleep(), async_runner=True)
+    register_runner(track.OperationType.Sleep, Sleep(), async_runner=True)
     # these requests should not be retried as they are not idempotent
-    register_runner(track.OperationType.CreateSnapshot.name, CreateSnapshot(), async_runner=True)
-    register_runner(track.OperationType.RestoreSnapshot.name, RestoreSnapshot(), async_runner=True)
+    register_runner(track.OperationType.CreateSnapshot, CreateSnapshot(), async_runner=True)
+    register_runner(track.OperationType.RestoreSnapshot, RestoreSnapshot(), async_runner=True)
     # We treat the following as administrative commands and thus already start to wrap them in a retry.
-    register_runner(track.OperationType.ClusterHealth.name, Retry(ClusterHealth()), async_runner=True)
-    register_runner(track.OperationType.PutPipeline.name, Retry(PutPipeline()), async_runner=True)
-    register_runner(track.OperationType.Refresh.name, Retry(Refresh()), async_runner=True)
-    register_runner(track.OperationType.CreateIndex.name, Retry(CreateIndex()), async_runner=True)
-    register_runner(track.OperationType.DeleteIndex.name, Retry(DeleteIndex()), async_runner=True)
-    register_runner(track.OperationType.CreateComponentTemplate.name, Retry(CreateComponentTemplate()), async_runner=True)
-    register_runner(track.OperationType.DeleteComponentTemplate.name, Retry(DeleteComponentTemplate()), async_runner=True)
-    register_runner(track.OperationType.CreateComposableTemplate.name, Retry(CreateComposableTemplate()), async_runner=True)
-    register_runner(track.OperationType.DeleteComposableTemplate.name, Retry(DeleteComposableTemplate()), async_runner=True)
-    register_runner(track.OperationType.CreateDataStream.name, Retry(CreateDataStream()), async_runner=True)
-    register_runner(track.OperationType.DeleteDataStream.name, Retry(DeleteDataStream()), async_runner=True)
-    register_runner(track.OperationType.CreateIndexTemplate.name, Retry(CreateIndexTemplate()), async_runner=True)
-    register_runner(track.OperationType.DeleteIndexTemplate.name, Retry(DeleteIndexTemplate()), async_runner=True)
-    register_runner(track.OperationType.ShrinkIndex.name, Retry(ShrinkIndex()), async_runner=True)
-    register_runner(track.OperationType.CreateMlDatafeed.name, Retry(CreateMlDatafeed()), async_runner=True)
-    register_runner(track.OperationType.DeleteMlDatafeed.name, Retry(DeleteMlDatafeed()), async_runner=True)
-    register_runner(track.OperationType.StartMlDatafeed.name, Retry(StartMlDatafeed()), async_runner=True)
-    register_runner(track.OperationType.StopMlDatafeed.name, Retry(StopMlDatafeed()), async_runner=True)
-    register_runner(track.OperationType.CreateMlJob.name, Retry(CreateMlJob()), async_runner=True)
-    register_runner(track.OperationType.DeleteMlJob.name, Retry(DeleteMlJob()), async_runner=True)
-    register_runner(track.OperationType.OpenMlJob.name, Retry(OpenMlJob()), async_runner=True)
-    register_runner(track.OperationType.CloseMlJob.name, Retry(CloseMlJob()), async_runner=True)
-    register_runner(track.OperationType.DeleteSnapshotRepository.name, Retry(DeleteSnapshotRepository()), async_runner=True)
-    register_runner(track.OperationType.CreateSnapshotRepository.name, Retry(CreateSnapshotRepository()), async_runner=True)
-    register_runner(track.OperationType.WaitForSnapshotCreate.name, Retry(WaitForSnapshotCreate()), async_runner=True)
-    register_runner(track.OperationType.WaitForRecovery.name, Retry(IndicesRecovery()), async_runner=True)
-    register_runner(track.OperationType.PutSettings.name, Retry(PutSettings()), async_runner=True)
-    register_runner(track.OperationType.CreateTransform.name, Retry(CreateTransform()), async_runner=True)
-    register_runner(track.OperationType.StartTransform.name, Retry(StartTransform()), async_runner=True)
-    register_runner(track.OperationType.WaitForTransform.name, Retry(WaitForTransform()), async_runner=True)
-    register_runner(track.OperationType.DeleteTransform.name, Retry(DeleteTransform()), async_runner=True)
+    register_runner(track.OperationType.ClusterHealth, Retry(ClusterHealth()), async_runner=True)
+    register_runner(track.OperationType.PutPipeline, Retry(PutPipeline()), async_runner=True)
+    register_runner(track.OperationType.Refresh, Retry(Refresh()), async_runner=True)
+    register_runner(track.OperationType.CreateIndex, Retry(CreateIndex()), async_runner=True)
+    register_runner(track.OperationType.DeleteIndex, Retry(DeleteIndex()), async_runner=True)
+    register_runner(track.OperationType.CreateComponentTemplate, Retry(CreateComponentTemplate()), async_runner=True)
+    register_runner(track.OperationType.DeleteComponentTemplate, Retry(DeleteComponentTemplate()), async_runner=True)
+    register_runner(track.OperationType.CreateComposableTemplate, Retry(CreateComposableTemplate()), async_runner=True)
+    register_runner(track.OperationType.DeleteComposableTemplate, Retry(DeleteComposableTemplate()), async_runner=True)
+    register_runner(track.OperationType.CreateDataStream, Retry(CreateDataStream()), async_runner=True)
+    register_runner(track.OperationType.DeleteDataStream, Retry(DeleteDataStream()), async_runner=True)
+    register_runner(track.OperationType.CreateIndexTemplate, Retry(CreateIndexTemplate()), async_runner=True)
+    register_runner(track.OperationType.DeleteIndexTemplate, Retry(DeleteIndexTemplate()), async_runner=True)
+    register_runner(track.OperationType.ShrinkIndex, Retry(ShrinkIndex()), async_runner=True)
+    register_runner(track.OperationType.CreateMlDatafeed, Retry(CreateMlDatafeed()), async_runner=True)
+    register_runner(track.OperationType.DeleteMlDatafeed, Retry(DeleteMlDatafeed()), async_runner=True)
+    register_runner(track.OperationType.StartMlDatafeed, Retry(StartMlDatafeed()), async_runner=True)
+    register_runner(track.OperationType.StopMlDatafeed, Retry(StopMlDatafeed()), async_runner=True)
+    register_runner(track.OperationType.CreateMlJob, Retry(CreateMlJob()), async_runner=True)
+    register_runner(track.OperationType.DeleteMlJob, Retry(DeleteMlJob()), async_runner=True)
+    register_runner(track.OperationType.OpenMlJob, Retry(OpenMlJob()), async_runner=True)
+    register_runner(track.OperationType.CloseMlJob, Retry(CloseMlJob()), async_runner=True)
+    register_runner(track.OperationType.DeleteSnapshotRepository, Retry(DeleteSnapshotRepository()), async_runner=True)
+    register_runner(track.OperationType.CreateSnapshotRepository, Retry(CreateSnapshotRepository()), async_runner=True)
+    register_runner(track.OperationType.WaitForSnapshotCreate, Retry(WaitForSnapshotCreate()), async_runner=True)
+    register_runner(track.OperationType.WaitForRecovery, Retry(IndicesRecovery()), async_runner=True)
+    register_runner(track.OperationType.PutSettings, Retry(PutSettings()), async_runner=True)
+    register_runner(track.OperationType.CreateTransform, Retry(CreateTransform()), async_runner=True)
+    register_runner(track.OperationType.StartTransform, Retry(StartTransform()), async_runner=True)
+    register_runner(track.OperationType.WaitForTransform, Retry(WaitForTransform()), async_runner=True)
+    register_runner(track.OperationType.DeleteTransform, Retry(DeleteTransform()), async_runner=True)
 
 
 def runner_for(operation_type):
@@ -87,6 +88,9 @@ def runner_for(operation_type):
 def register_runner(operation_type, runner, **kwargs):
     logger = logging.getLogger(__name__)
     async_runner = kwargs.get("async_runner", False)
+    if isinstance(operation_type, track.OperationType):
+        operation_type = operation_type.to_hyphenated_string()
+
     if not async_runner:
         raise exceptions.RallyAssertionError(
             "Runner [{}] must be implemented as async runner and registered with async_runner=True.".format(str(runner)))
@@ -1969,6 +1973,50 @@ class DeleteTransform(Runner):
 
     def __repr__(self, *args, **kwargs):
         return "delete-transform"
+
+
+class Composite(Runner):
+    """
+    Executes a complex request structure which is measured by Rally as one composite operation.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.supported_op_types = ["raw-request", "sleep"]
+
+    async def run_stream(self, es, stream, connection_limit):
+        streams = []
+        try:
+            for item in stream:
+                if "stream" in item:
+                    streams.append(asyncio.create_task(self.run_stream(es, item["stream"], connection_limit)))
+                elif "operation-type" in item:
+                    op_type = item["operation-type"]
+                    if op_type not in self.supported_op_types:
+                        raise exceptions.RallyAssertionError(
+                            f"Unsupported operation-type [{op_type}]. Use one of [{', '.join(self.supported_op_types)}].")
+                    runner = runner_for(op_type)
+                    async with connection_limit:
+                        async with runner:
+                            await runner({"default": es}, item)
+                else:
+                    raise exceptions.RallyAssertionError("Requests structure must contain [stream] or [operation-type].")
+        except BaseException:
+            # stop all already created tasks in case of exceptions
+            for s in streams:
+                if not s.done():
+                    s.cancel()
+            raise
+
+        if streams:
+            await asyncio.gather(*streams)
+
+    async def __call__(self, es, params):
+        requests = mandatory(params, "requests", self)
+        max_connections = params.get("max-connections", sys.maxsize)
+        await self.run_stream(es, requests, asyncio.BoundedSemaphore(max_connections))
+
+    def __repr__(self, *args, **kwargs):
+        return "composite"
 
 
 # TODO: Allow to use this from (selected) regular runners and add user documentation.

esrally/track/params.py
@@ -59,7 +59,7 @@ def ensure_valid_param_source(param_source):
 
 def register_param_source_for_operation(op_type, param_source_class):
     ensure_valid_param_source(param_source_class)
-    __PARAM_SOURCES_BY_OP[op_type.name] = param_source_class
+    __PARAM_SOURCES_BY_OP[op_type.to_hyphenated_string()] = param_source_class
 
 
 def register_param_source_for_name(name, param_source_class):

commit 176dd95849ece9eb1182c218f8e1c7d0c782c205

Ensure admin tasks are always executed
When a user specified `--exclude-tasks`, Rally did not only exclude the
specified tasks but also all administrative tasks. This happened because
of a broken logic that always added an admin task filter internally. Due
to another bug, administrative tasks where never correctly detected but
we fixed that broken logic in #1112, which finally uncovered this issue.

With this commit we ensure now that administrative tasks are always
executed regardless whether the user excludes them. We also increase
test coverage in this part of the code.

esrally/track/loader.py
@@ -693,27 +693,31 @@ def filter_tasks(t, filters, exclude=False):
 
     logger = logging.getLogger(__name__)
 
-    def filter_out_match(task, filters, exclude):
-        for f in filters:
+    def filter_out_match(task, user_defined_filters, force_include_filters, exclude):
+        for f in force_include_filters:
             if task.matches(f):
-                if hasattr(task, 'tasks') and exclude:
+                return False
+
+        for f in user_defined_filters:
+            if task.matches(f):
+                if hasattr(task, "tasks") and exclude:
                     return False
                 return exclude
         return not exclude
 
     # always include administrative tasks
-    complete_filters = [track.AdminTaskFilter()] + filters
+    force_include_filters = [track.AdminTaskFilter()]
 
     for challenge in t.challenges:
         # don't modify the schedule while iterating over it
         tasks_to_remove = []
         for task in challenge.schedule:
-            if filter_out_match(task, complete_filters, exclude):
+            if filter_out_match(task, filters, force_include_filters, exclude):
                 tasks_to_remove.append(task)
             else:
                 leafs_to_remove = []
                 for leaf_task in task:
-                    if filter_out_match(leaf_task, complete_filters, exclude):
+                    if filter_out_match(leaf_task, filters, force_include_filters, exclude):
                         leafs_to_remove.append(leaf_task)
                 for leaf_task in leafs_to_remove:
                     logger.info("Removing sub-task [%s] from challenge [%s] due to task filter.", leaf_task, challenge)

tests/track/loader_test.py
@@ -1313,6 +1313,10 @@ class TrackFilterTests(TestCase):
             "indices": [{"name": "test-index", "auto-managed": False}],
             "operations": [
                 {
+                    "name": "create-index",
+                    "operation-type": "create-index"
+                },
+                {
                     "name": "bulk-index",
                     "operation-type": "bulk"
                 },
@@ -1338,6 +1342,9 @@ class TrackFilterTests(TestCase):
                 {
                     "name": "default-challenge",
                     "schedule": [
+                        {
+                            "operation": "create-index"
+                        },
                         {
                             "parallel": {
                                 "tasks": [
@@ -1376,7 +1383,7 @@ class TrackFilterTests(TestCase):
         }
         reader = loader.TrackSpecificationReader()
         full_track = reader("unittest", track_specification, "/mappings")
-        self.assertEqual(4, len(full_track.challenges[0].schedule))
+        self.assertEqual(5, len(full_track.challenges[0].schedule))
 
         filtered = loader.filter_tasks(full_track, [track.TaskNameFilter("index-3"),
                                                              track.TaskOpTypeFilter("search"),
@@ -1385,16 +1392,21 @@ class TrackFilterTests(TestCase):
                                                              ])
 
         schedule = filtered.challenges[0].schedule
-        self.assertEqual(3, len(schedule))
-        self.assertEqual(["index-3", "match-all-parallel"], [t.name for t in schedule[0].tasks])
-        self.assertEqual("match-all-serial", schedule[1].name)
-        self.assertEqual("cluster-stats", schedule[2].name)
+        self.assertEqual(4, len(schedule))
+        self.assertEqual("create-index", schedule[0].name)
+        self.assertEqual(["index-3", "match-all-parallel"], [t.name for t in schedule[1].tasks])
+        self.assertEqual("match-all-serial", schedule[2].name)
+        self.assertEqual("cluster-stats", schedule[3].name)
 
     def test_filters_exclude_tasks(self):
         track_specification = {
             "description": "description for unit test",
             "indices": [{"name": "test-index", "auto-managed": False}],
             "operations": [
+                {
+                    "name": "create-index",
+                    "operation-type": "create-index"
+                },
                 {
                     "name": "bulk-index",
                     "operation-type": "bulk"
@@ -1421,6 +1433,9 @@ class TrackFilterTests(TestCase):
                 {
                     "name": "default-challenge",
                     "schedule": [
+                        {
+                            "operation": "create-index"
+                        },
                         {
                             "parallel": {
                                 "tasks": [
@@ -1459,15 +1474,16 @@ class TrackFilterTests(TestCase):
         }
         reader = loader.TrackSpecificationReader()
         full_track = reader("unittest", track_specification, "/mappings")
-        self.assertEqual(4, len(full_track.challenges[0].schedule))
+        self.assertEqual(5, len(full_track.challenges[0].schedule))
 
         filtered = loader.filter_tasks(full_track, [track.TaskNameFilter("index-3"), track.TaskOpTypeFilter("search")], exclude=True)
 
         schedule = filtered.challenges[0].schedule
-        self.assertEqual(3, len(schedule))
-        self.assertEqual(["index-1",'index-2'], [t.name for t in schedule[0].tasks])
-        self.assertEqual("node-stats", schedule[1].name)
-        self.assertEqual("cluster-stats", schedule[2].name)
+        self.assertEqual(4, len(schedule))
+        self.assertEqual("create-index", schedule[0].name)
+        self.assertEqual(["index-1", "index-2"], [t.name for t in schedule[1].tasks])
+        self.assertEqual("node-stats", schedule[2].name)
+        self.assertEqual("cluster-stats", schedule[3].name)
 
 
 class TrackSpecificationReaderTests(TestCase):

tests/track/track_test.py
@@ -231,6 +231,33 @@ class OperationTypeTests(TestCase):
             self.assertEqual(op_type, track.OperationType.from_hyphenated_string(op_type.to_hyphenated_string()))
 
 
+class TaskFilterTests(TestCase):
+    def create_index_task(self):
+        return track.Task("create-index-task",
+                          track.Operation("create-index-op",
+                                          operation_type=track.OperationType.CreateIndex.to_hyphenated_string()))
+
+    def search_task(self):
+        return track.Task("search-task",
+                          track.Operation("search-op",
+                                          operation_type=track.OperationType.Search.to_hyphenated_string()))
+
+    def test_admin_task_filter(self):
+        f = track.AdminTaskFilter()
+        self.assertTrue(f.matches(self.create_index_task()))
+        self.assertFalse(f.matches(self.search_task()))
+
+    def test_task_name_filter(self):
+        f = track.TaskNameFilter("create-index-task")
+        self.assertTrue(f.matches(self.create_index_task()))
+        self.assertFalse(f.matches(self.search_task()))
+
+    def test_task_op_type_filter(self):
+        f = track.TaskOpTypeFilter(track.OperationType.CreateIndex.to_hyphenated_string())
+        self.assertTrue(f.matches(self.create_index_task()))
+        self.assertFalse(f.matches(self.search_task()))
+
+
 class TaskTests(TestCase):
     def task(self, schedule=None, target_throughput=None, target_interval=None):
         op = track.Operation("bulk-index", track.OperationType.Bulk.name)