b40abf52003ba7cd13f7c20dd6877dd66d9036e6
Travis Benedict <benedtra@amazon.com>
2021-06-08 14:01:46 -0500

    Remove pyenv init

--- .ci/build.sh
+++ .ci/build.sh
@@ -43,7 +43,6 @@ function build {
   export TERM=dumb
   export LC_ALL=en_US.UTF-8
   update_pyenv
-  eval "$(pyenv init -)"
   # ensure pyenv shims are added to PATH, see https://github.com/pyenv/pyenv/issues/1906
   eval "$(pyenv init --path)"
   eval "$(pyenv virtualenv-init -)"
