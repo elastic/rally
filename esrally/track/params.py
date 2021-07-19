3a6a2aec3bf6f0c53bd1336826668c3329b49f84
Dale McDiarmid <dale@elastic.co>
2021-07-19 15:04:08 +0100

    Fix deleting component/composable templates

--- esrally/track/params.py
+++ esrally/track/params.py
@@ -395,15 +395,15 @@ class DeleteIndexTemplateParamSource(ParamSource):
         return p
 
 
-class DeleteComponentTemplateParamSource(ParamSource):
-    def __init__(self, track, params, **kwargs):
+class DeleteTemplateParamSource(ABC, ParamSource):
+    def __init__(self, track, params, templates, **kwargs):
         super().__init__(track, params, **kwargs)
         self.only_if_exists = params.get("only-if-exists", True)
         self.request_params = params.get("request-params", {})
         self.template_definitions = []
-        if track.templates:
+        if templates:
             filter_template = params.get("template")
-            for template in track.templates:
+            for template in templates:
                 if not filter_template or template.name == filter_template:
                     self.template_definitions.append(template.name)
         else:
@@ -419,6 +419,16 @@ class DeleteComponentTemplateParamSource(ParamSource):
             "only-if-exists": self.only_if_exists,
             "request-params": self.request_params,
         }
+
+
+class DeleteComposableTemplateParamSource(DeleteTemplateParamSource):
+    def __init__(self, track, params, **kwargs):
+        super().__init__(track, params, track.composable_templates, **kwargs)
+
+
+class DeleteComponentTemplateParamSource(DeleteTemplateParamSource):
+    def __init__(self, track, params, **kwargs):
+        super().__init__(track, params, track.component_templates, **kwargs)
 
 
 class CreateTemplateParamSource(ABC, ParamSource):
@@ -1323,7 +1333,7 @@ register_param_source_for_operation(track.OperationType.DeleteIndexTemplate, Del
 register_param_source_for_operation(track.OperationType.CreateComponentTemplate, CreateComponentTemplateParamSource)
 register_param_source_for_operation(track.OperationType.DeleteComponentTemplate, DeleteComponentTemplateParamSource)
 register_param_source_for_operation(track.OperationType.CreateComposableTemplate, CreateComposableTemplateParamSource)
-register_param_source_for_operation(track.OperationType.DeleteComposableTemplate, DeleteIndexTemplateParamSource)
+register_param_source_for_operation(track.OperationType.DeleteComposableTemplate, DeleteComposableTemplateParamSource)
 register_param_source_for_operation(track.OperationType.Sleep, SleepParamSource)
 register_param_source_for_operation(track.OperationType.ForceMerge, ForceMergeParamSource)
 
