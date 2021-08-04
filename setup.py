df9ac828924aa679792605d5606a4bc8f5f8f81e
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2021-08-04 07:49:26 +0200

    Upgrade client to 7.14.0

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ install_requires = [
     # transitive dependencies:
     #   urllib3: MIT
     #   aiohttp: Apache 2.0
-    "elasticsearch[async]==7.14.0a2",
+    "elasticsearch[async]==7.14.0",
     # License: BSD
     "psutil==5.8.0",
     # License: MIT
