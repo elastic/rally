0c63c25fbd8c6293bbd1cd7555bcf78bc6ca2600
Brad Deam <bradley.deam@elastic.co>
2021-08-16 19:12:12 +0930

    Update config file location variable

--- tests/metrics_test.py
+++ tests/metrics_test.py
@@ -2417,7 +2417,7 @@ class IndexTemplateProviderTests(TestCase):
         with self.assertRaisesRegex(
             expected_exception=exceptions.SystemSetupError,
             expected_regex=f"The setting: datastore.number_of_shards must be >= 1. Please check the configuration in "
-            f"{config.ConfigFile().location}/rally.ini",
+            f"{_index_template_provider._config.config_file.location}/rally.ini",
         ):
             # pylint: disable=unused-variable
             templates = [
