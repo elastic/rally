cae6b66c635b243c2c0cf7ebd32de0ff2a8a26d6
Nik Everett <nik9000@gmail.com>
2022-03-01 16:38:48 -0500

    Tests for more things

--- tests/utils/convert_test.py
+++ tests/utils/convert_test.py
@@ -37,3 +37,31 @@ class TestToBool:
             with pytest.raises(ValueError) as exc:
                 convert.to_bool(value)
             assert exc.value.args[0] == f"Cannot convert [{value}] to bool."
+
+class TestBytesToHumanString:
+    def test_none(self):
+        assert convert.bytes_to_human_string(None) == "N/A"
+
+    def test_positive_bytes(self):
+        assert convert.bytes_to_human_string(100) == "100 bytes"
+
+    def test_negative_bytes(self):
+        assert convert.bytes_to_human_string(-100) == "-100 bytes"
+
+    def test_positive_kb(self):
+        assert convert.bytes_to_human_string(8808) == "8.6 kB"
+
+    def test_negative_kb(self):
+        assert convert.bytes_to_human_string(-88134) == "-86.1 kB"
+
+    def test_positive_mb(self):
+        assert convert.bytes_to_human_string(8808812) == "8.4 MB"
+
+    def test_negative_mb(self):
+        assert convert.bytes_to_human_string(-881348666) == "-840.5 MB"
+
+    def test_positive_gb(self):
+        assert convert.bytes_to_human_string(8808812123) == "8.2 GB"
+
+    def test_negative_gb(self):
+        assert convert.bytes_to_human_string(-881348666323) == "-820.8 GB"
