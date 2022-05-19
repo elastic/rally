d98c4b6186eb774aad79fabcb03ffabde416add1
Quentin Pradet <quentin.pradet@elastic.co>
2022-05-19 11:05:56 +0400

    Fix use_ssl: True on Python 3.10 (#1493)
    Rally is a client, so its purpose is to authenticate servers. This means
    that we should use ssl.Purpose.SERVER_AUTH instead of CLIENT_AUTH, with
    or without client certs. This is super confusing, see
    python/cpython#73996. Anyway, in 3.10 making this mistake isn't possible
    anymore due python/cpython#26646.
        
    To make sure this does not break in the future, we also add tests for
    the IP and client certs cases.

--- setup.py
+++ setup.py
@@ -104,6 +104,8 @@ develop_require = [
     "pylint==2.6.0",
     "black==22.3.0",
     "isort==5.8.0",
+    "trustme==0.9.0",
+    "pytest-httpserver==1.0.4",
 ]
 
 python_version_classifiers = ["Programming Language :: Python :: {}.{}".format(major, minor) for major, minor in supported_python_versions]
