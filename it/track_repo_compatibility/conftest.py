f5190a1fa9802bd50b9d41b524b10b8c9444fcdd
Mike Baamonde <mike.baamonde@elastic.co>
2022-08-22 16:43:48 -0400

    Add ability to run track repo tests within the Rally repo.
    This commit modifies and extends `pytest-rally` so that it can be invoked from
    within the Rally repo. This enables Rally developers and CI jobs to test
    Rally changes against arbitrary revisions of local track repositories,
    using arbitrary versions of Elasticsearch.
    
    The actual contents of the tests live in the track repository. The
    plugin will run any tests found in the `it` subdirectory of the provided track
    repository by default, including those that are auto-generated by
    `pytest-rally`.
    
    By default, tests will be run against the `master` branch of the track
    repository checked out in `$RALLY_HOME/benchmarks/tracks/default` (typically
    `rally-tracks`), using a build of the `main` branch of Elasticsearch.
    
    To run tests with these defaults, run the following from the root of this
    repository:
    
    `pytest it/track_repo_compatibility`
    
    Here is an example invocation that overrides these defaults:
    
    ```
    pytest it/track_repo_compatibility \
         --track-repository=/path/to/repo \
         --track-revision=some-branch \
         --distribution-version=8.3.2
    ```

---
+++ it/track_repo_compatibility/conftest.py
@@ -0,0 +1,55 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# 	http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import os
+
+import pytest
+
+RALLY_HOME = os.getenv("RALLY_HOME", os.path.expanduser("~"))
+RALLY_CONFIG_DIR = os.path.join(RALLY_HOME, ".rally")
+RALLY_TRACKS_DIR = os.path.join(RALLY_CONFIG_DIR, "benchmarks", "tracks", "default")
+
+
+@pytest.hookimpl
+def pytest_addoption(parser):
+    group = parser.getgroup("rally")
+    group.addoption(
+        "--track-repository",
+        action="store",
+        default=RALLY_TRACKS_DIR,
+        help=("Path to a local track repository\n" f"(default: {RALLY_TRACKS_DIR})"),
+    )
+    group.addoption("--track-revision", action="store", default="master", help=("Track repository revision to test\n" "default: `master`"))
+    group.addoption(
+        "--track-repository-test-directory",
+        action="store",
+        dest="track_repo_test_dir",
+        default="it",
+        help=("Name of the directory containing the track repo's integration tests\n" "(default: `it`)"),
+    )
+
+
+@pytest.hookimpl
+def pytest_cmdline_main(config):
+    repo = config.option.track_repository
+    test_dir = config.option.track_repo_test_dir
+    config.args.append(os.path.join(repo, test_dir))
+
+
+@pytest.hookimpl
+def pytest_report_header(config):
+    return f"rally: track-repository={config.option.track_repository}, track-revision={config.option.track_revision}"
