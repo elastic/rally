257794baaf8cb4228196cc63c8ff7172bf2da86e
Mike Baamonde <mike.baamonde@elastic.co>
2022-08-24 13:20:11 -0400

    Explicitly clone rally-tracks into a distinct location by default.

--- it/track_repo_compatibility/conftest.py
+++ it/track_repo_compatibility/conftest.py
@@ -23,8 +23,8 @@ import pytest
 
 RALLY_HOME = os.getenv("RALLY_HOME", os.path.expanduser("~"))
 RALLY_CONFIG_DIR = os.path.join(RALLY_HOME, ".rally")
-RALLY_TRACKS_DIR = os.path.join(RALLY_CONFIG_DIR, "benchmarks", "tracks", "default")
-
+TRACK_REPO_PATH = os.path.join(RALLY_CONFIG_DIR, "benchmarks", "tracks", "rally-tracks-compat")
+REMOTE_TRACK_REPO = "https://github.com/elastic/rally-tracks"
 
 @pytest.hookimpl(tryfirst=True)
 def pytest_addoption(parser):
@@ -32,8 +32,8 @@ def pytest_addoption(parser):
     group.addoption(
         "--track-repository",
         action="store",
-        default=RALLY_TRACKS_DIR,
-        help=("Path to a local track repository\n" f"(default: {RALLY_TRACKS_DIR})"),
+        default=TRACK_REPO_PATH,
+        help=("Path to a local track repository\n" f"(default: {TRACK_REPO_PATH})"),
     )
     group.addoption("--track-revision", action="store", default="master", help=("Track repository revision to test\n" "default: `master`"))
     group.addoption(
@@ -49,12 +49,13 @@ def pytest_addoption(parser):
 def pytest_cmdline_main(config):
     repo = config.option.track_repository
     if not os.path.isdir(repo):
-        if repo == RALLY_TRACKS_DIR:
+        # we're using the defaults, so perform an initial clone of rally-tracks
+        if repo == TRACK_REPO_PATH:
             try:
-                # this will perform the initial clone of rally-tracks
-                subprocess.run(shlex.split("esrally list tracks"), text=True, capture_output=True, check=True)
+                subprocess.run(shlex.split(f"git clone {REMOTE_TRACK_REPO} {repo}"), text=True, capture_output=True, check=True)
             except subprocess.CalledProcessError as e:
-                raise AssertionError(f"Unable to list tracks in {repo}: {e.stderr}")
+                raise AssertionError(f"Unable to clone {REMOTE_TRACK_REPO} into {repo}: {e.stderr}")
+        # user has provided a non-default track repository, but it's not a valid path, so we raise an error
         else:
             raise AssertionError(f"Track repository not found: [{repo}]")
 
