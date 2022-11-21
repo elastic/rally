242cf406d6744e70e75ba0e4c35ee1a78d1f0569
Quentin Pradet <quentin.pradet@elastic.co>
2022-11-21 10:21:56 +0400

    Switch from pytoml to tomli
    tomli is maintained and support TOML v1.0.0. It does not really matter
    in this case but this code could be easily copy/pasted for other
    purposes.

--- it/resources/track_with_dependency/track.py
+++ it/resources/track_with_dependency/track.py
@@ -1,13 +1,13 @@
 async def noop(es, params):
     # pylint: disable=import-outside-toplevel,import-error
-    import pytoml as toml
+    import tomli as tomllib
 
     ret_val = {}
     # no-ops is our correctness marker
     toml_values = ["weight = 1", 'unit = "no-ops"']
     await es.cluster.health()
     for toml_value in toml_values:
-        ret_val.update(toml.loads(toml_value))
+        ret_val.update(tomllib.loads(toml_value))
     return ret_val
 
 
