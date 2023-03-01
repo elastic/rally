caeb65481787f4b3ffc0a7fa0d03589ee9233117
Quentin Pradet <quentin.pradet@elastic.co>
2023-03-01 16:56:37 +0400

    Run rally-tracks-compat in GitHub Actions

--- noxfile.py
+++ noxfile.py
@@ -11,3 +11,10 @@ def test(session: nox.Session) -> None:
 def it(session: nox.Session) -> None:
     session.install(".[develop]")
     session.run("pytest", "-s", "it")
+
+
+@nox.session(python="3")
+def rally_tracks_compat(session: nox.Session) -> None:
+    session.install(".[develop]")
+    session.install("pytest-rally @ git+https://github.com/elastic/pytest-rally.git")
+    session.run("pytest", "it/track_repo_compatibility", "--log-cli-level=INFO")
