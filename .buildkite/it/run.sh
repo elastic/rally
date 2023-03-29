899cc853a281cd79801e6e580b22a5826e7c8e40
Quentin Pradet <quentin.pradet@elastic.co>
2023-03-29 10:21:28 +0400

    Prevent Ubuntu 22.04 restart prompt

--- .buildkite/it/run.sh
+++ .buildkite/it/run.sh
@@ -4,8 +4,10 @@ set -eo pipefail
 
 export TERM=dumb
 export LC_ALL=en_US.UTF-8
-export DEBIAN_FRONTEND=noninteractive
 export TZ=Etc/UTC
+# https://askubuntu.com/questions/1367139/apt-get-upgrade-auto-restart-services
+export DEBIAN_FRONTEND=noninteractive
+export NEEDRESTART_MODE=a
 
 # based on https://gist.github.com/sj26/88e1c6584397bb7c13bd11108a579746?permalink_comment_id=4155247#gistcomment-4155247
 function retry {
