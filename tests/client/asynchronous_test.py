9fb6ab1b0c12cdcdf549a7e10635cfac24394f6b
Brad Deam <bradley.deam@elastic.co>
2023-04-21 14:51:03 +0930

    Evenly distribute client connections across hostnames that resolve to multiple IP addresses

--- tests/client/asynchronous_test.py
+++ tests/client/asynchronous_test.py
@@ -16,8 +16,13 @@
 # under the License.
 
 import json
+import math
+import random
+from unittest import mock
 
-from esrally.client.asynchronous import ResponseMatcher
+import pytest
+
+from esrally.client.asynchronous import RallyTCPConnector, ResponseMatcher
 
 
 class TestResponseMatcher:
@@ -49,3 +54,76 @@ class TestResponseMatcher:
     def assert_response_type(self, matcher, path, expected_response_type):
         response = json.loads(matcher.response(path))
         assert response["response-type"] == expected_response_type
+
+
+class TestResolveHost:
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize(
+        "num_of_dns_resp, num_clients",
+        [(1, 256), (2, 128), (3, 1), (3, 64), (4, 16), (3, 2000)],
+    )
+    @mock.patch("esrally.client.asynchronous.aiohttp.ThreadedResolver.resolve")
+    async def test_resolve_host_even_client_allocation(
+        self,
+        mocked_resolver,
+        num_of_dns_resp,
+        num_clients,
+    ):
+        """
+        TCPConnector._resolve_host() returns all the IPs a given name resolves to, but the underlying connection but the
+        logic in TCPConnector._create_direct_connection() only ever selects the first succesful host from this list.
+
+        So, we use the factory assigned client_id to deterministically return an IP and then place it at the beginning
+        of the list to evenly distributes connections across _all_ clients.
+
+        https://github.com/elastic/rally/issues/1598
+        """
+
+        def generate_dns_response(num_of_dns_resp):
+            dns_resp = []
+            for _ in range(num_of_dns_resp):
+                dns_resp.append(
+                    {
+                        "hostname": "rally-dns-test.es.us-east-1.aws.found.io",
+                        "host": ".".join([str(random.randint(0, 255)) for _ in range(4)]),
+                        "port": 443,
+                        "family": "test-family",
+                        "proto": 6,
+                        "flags": "test-flag",
+                    },
+                )
+            return dns_resp
+
+        dns_responses = generate_dns_response(num_of_dns_resp)
+        mocked_resolver.return_value = dns_responses
+
+        hostinfo = []
+        for i in range(num_clients):
+            hostinfo.append(
+                # pylint: disable=protected-access
+                await RallyTCPConnector(limit_per_host=256, use_dns_cache=True, enable_cleanup_closed=True, client_id=i)._resolve_host(
+                    "rally-dns-test.es.us-east-1.aws.found.io", 443
+                )
+            )
+
+        first_host_per_client = []
+        for host in hostinfo:
+            # for each host extract the first 'host' resolved as that's what will be used to establish a connection
+            first_host_per_client.append(host[0]["host"])
+
+        # count the distribution of the allocation
+        ip_alloc = {}
+        for ip in first_host_per_client:
+            if ip in ip_alloc:
+                ip_alloc[ip] += 1
+            else:
+                ip_alloc[ip] = 1
+
+        # maximum and minimum number of clients a single ip should have
+        upper_bound = math.ceil(num_clients / num_of_dns_resp)
+        lower_bound = math.floor(num_clients / num_of_dns_resp)
+
+        for num_of_clients in ip_alloc.values():
+            assert num_of_clients == upper_bound or lower_bound
+
+        assert sum(ip_alloc.values()) == num_clients
