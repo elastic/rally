b8c3c8c3b35dfa23f4ff9367a37f3fe5c4568240
Grzegorz Banasiak <grzegorz.banasiak@elastic.co>
2023-07-05 10:53:52 +0200

    Test GH notifications
    This commit will be reverted after test.

---
+++ test-gh-notifications.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# this file does not follow formatting rules
+# there are no 2 empty lines before/after main()
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
