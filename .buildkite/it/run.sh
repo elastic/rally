4f5e50f7039cd73eaa1519727347a1a235f98fac
Quentin Pradet <quentin.pradet@elastic.co>
2023-09-07 11:31:59 +0400

    Extract retry function in a file

--- .buildkite/it/run.sh
+++ .buildkite/it/run.sh
@@ -2,6 +2,8 @@
 
 set -eo pipefail
 
+source .buildkite/retry.sh
+
 export TERM=dumb
 export LC_ALL=en_US.UTF-8
 export TZ=Etc/UTC
@@ -10,37 +12,6 @@ export DEBIAN_FRONTEND=noninteractive
 sudo mkdir -p /etc/needrestart
 echo "\$nrconf{restart} = 'a';" | sudo tee -a /etc/needrestart/needrestart.conf > /dev/null
 
-# based on https://gist.github.com/sj26/88e1c6584397bb7c13bd11108a579746?permalink_comment_id=4155247#gistcomment-4155247
-function retry {
-  local retries=$1
-  shift
-  local cmd=($@)
-  local cmd_string="${@}"
-  local count=0
-
-  # be lenient with non-zero exit codes, to allow retries
-  set +o errexit
-  set +o pipefail
-  until "${cmd[@]}"; do
-    retcode=$?
-    wait=$(( 2 ** count ))
-    count=$(( count + 1))
-    if [[ $count -le $retries ]]; then
-      printf "Command [%s] failed. Retry [%d/%d] in [%d] seconds.\n" "$cmd_string" $count $retries $wait
-      sleep $wait
-    else
-      printf "Exhausted all [%s] retries for command [%s]. Exiting.\n" "$cmd_string" $retries
-      # restore settings to fail immediately on error
-      set -o errexit
-      set -o pipefail
-      return $retcode
-    fi
-  done
-  # restore settings to fail immediately on error
-  set -o errexit
-  set -o pipefail
-  return 0
-}
 echo "--- System dependencies"
 
 PYTHON_VERSION="$1"
