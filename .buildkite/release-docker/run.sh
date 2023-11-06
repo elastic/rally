01ee207db628dca235f8127fe19008c56fa20d52
Grzegorz Banasiak <grzegorz.banasiak@elastic.co>
2023-11-06 21:33:08 +0100

    Checkout tag on Docker release (#1799)

--- .buildkite/release-docker/run.sh
+++ .buildkite/release-docker/run.sh
@@ -11,7 +11,18 @@ DOCKER_PASSWORD=$(vault read -field token /secret/ci/elastic-rally/release/docke
 retry 5 docker login -u elasticmachine -p $DOCKER_PASSWORD
 unset DOCKER_PASSWORD
 
+tmp_dir=$(mktemp --directory)
+pushd "$tmp_dir"
+git clone https://github.com/elastic/rally
+pushd rally
+git checkout "${RELEASE_VERSION}"
+git --no-pager show
+
 set -x
 export TERM=dumb
 export LC_ALL=en_US.UTF-8
 ./release-docker.sh "$RELEASE_VERSION"
+
+popd
+popd
+rm -rf "$tmp_dir"
