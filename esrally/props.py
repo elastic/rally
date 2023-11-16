0935e90dfbb6bcccb801ff2d3188061cfd81a805
Youhei Sakurai <sakurai.youhei@gmail.com>
2023-11-16 19:30:09 +0900

    Add comments

--- esrally/props.py
+++ esrally/props.py
@@ -17,28 +17,28 @@
 
 from enum import Enum, auto
 
+# TODO remove ImportError fallback when we drop support for Python 3.10
 try:
-    from enum import EnumType
+    from enum import EnumType, member
 except ImportError:
     from enum import EnumMeta as EnumType
-
-try:
-    from enum import member
-except ImportError:
 
     def member(enum):
         return enum
 
 
 class PropEnumType(EnumType):
-    __iter__ = None
+    __iter__ = None  # Disallows unintentional dot notation
 
 
 class PropEnum(str, Enum, metaclass=PropEnumType):
+    """StrEnum-like Enum but supports multi-level nesting"""
+
     def __new__(cls, value):
         if isinstance(value, PropEnumType):
-            return value
+            return value  # Nested Enum should stay original
         elif isinstance(value, str):
+            # This prefixing lets A.B.C.D values not just "d" but "a.b.c.d"
             value = f"{cls.__qualname__.lower()}.{value}"
             member = str.__new__(cls, value)
             member._value_ = value
@@ -53,6 +53,7 @@ class PropEnum(str, Enum, metaclass=PropEnumType):
 
 class Property(PropEnum):
     def __sections(self):
+        """Yields section names from the top level to the bottom"""
         node = globals()
         for name in type(self).__qualname__.split("."):
             node = node[name]
@@ -70,6 +71,7 @@ class Property(PropEnum):
         return self.value.replace(f"{self.__section}.", "", 1)
 
     def __iter__(self):
+        """Allows to unpack pair of section and key names by star expansion"""
         return iter(
             (
                 self.__section,
@@ -90,6 +92,15 @@ class Key(Property):
 
 
 def desc(*_):
+    """Just a blank function to write a description for each key as a string
+
+    The usage of this function is open-ended for future enhancements. By main-
+    taining descriptions as (list of) str in the source code, it may be
+    possible to reuse them as e.g. help texts for users in the future.
+
+    Returns `enum.auto()` to let Enum populate an appropriate value to the
+    member.
+    """
     return auto()
 
 
@@ -100,9 +111,23 @@ class SYSTEM(Section):
 
     @member
     class LIST(Key):
-        FROM_DATE = desc()
-        MAX_RESULTS = desc()
-        TO_DATE = desc()
+        FROM_DATE = desc(
+            """
+            list records only from this date
+            """
+        )
+
+        MAX_RESULTS = desc(
+            """
+            the limit of the number of records to return
+            """
+        )
+
+        TO_DATE = desc(
+            """
+            list records only to this date
+            """
+        )
 
         @member
         class CONFIG(Key):
