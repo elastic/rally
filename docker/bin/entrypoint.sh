e00465632dfa8138859f0f6adc9cdf06c007244e
favilo <kevin.oberlies@elastic.co>
2024-10-10 14:32:42 -0700

    test: Start waiting for the esrallyd children processes to exit. This will allow us to actually run the thespian children inside docker.

--- docker/bin/entrypoint.sh
+++ docker/bin/entrypoint.sh
@@ -3,6 +3,16 @@ set -Eeo pipefail
 
 if [[ $1 == *"bash"* || $1 == *"sh"* ]]; then
     : # noop
+elif [[ $1 == "esrallyd" && $2 == "start" ]]; then
+    # We want to wait for esrallyd's child processes to exit before we exit
+    set -x
+    "$@" &
+    pid=$!
+    ps -e
+    child_pids=$(pgrep -P $pid)
+    echo "Started $1 with PID $pid. Waiting for child processes $child_pids to exit."
+    wait "$child_pids"
+    exit 0
 elif [[ $1 != "esrally" ]]; then
     set -- esrally "$@"
 fi
