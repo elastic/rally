8c1777f17d9928affe52914a110d318dc112f39c
favilo <kevin.oberlies@elastic.co>
2024-10-11 10:36:17 -0700

    revert: Revert entrypoint.sh
    It'll be easier to wait for the child processes from python
    when RALLY_RUNNING_IN_DOCKER is set. And we can just set the docker
    command to include `--daemon` to make sure that we don't block on the
    command running.

--- docker/bin/entrypoint.sh
+++ docker/bin/entrypoint.sh
@@ -3,20 +3,6 @@ set -Eeo pipefail
 
 if [[ $1 == *"bash"* || $1 == *"sh"* ]]; then
     : # noop
-elif [[ $1 == "esrallyd" && $2 == "start" ]]; then
-    # We want to wait for esrallyd's child processes to exit before we exit
-    set -x
-    echo "Starting $1"
-    echo "$@"
-    shift
-    echo "$@"
-    "$@" &
-    pid=$!
-    ps -e
-    child_pids=$(pgrep -P $pid)
-    echo "Started $1 with PID $pid. Waiting for child processes $child_pids to exit."
-    wait "$child_pids"
-    exit 0
 elif [[ $1 != "esrally" ]]; then
     set -- esrally "$@"
 fi
