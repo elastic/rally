3bb3763400e280c9e68a9e7d9460db001ad2fa58
Federico Ressi <federico.ressi@elastic.co>
2025-06-17 16:08:42 +0200

    Add testing for MirroList.from_config method and improve testing for the resolve method.

--- tests/storage/mirror_test.py
+++ tests/storage/mirror_test.py
@@ -19,42 +19,68 @@ from __future__ import annotations
 import os.path
 from dataclasses import dataclass
 
-import pytest
-
 from esrally.config import Config, Scope
 from esrally.storage._mirror import MirrorList
+from esrally.types import Key
 from esrally.utils.cases import cases
 
-BASE_URL = "https://rally-tracks.elastic.co/"
+BASE_URL = "https://rally-tracks.elastic.co"
 SOME_PATH = "some/file.json.bz2"
 URL = f"{BASE_URL}/{SOME_PATH}"
 MIRROR1_URL = "https://rally-tracks-eu-central-1.s3.eu-central-1.amazonaws.com"
 MIRROR2_URL = "https://rally-tracks-us-west-1.s3.us-west-1.amazonaws.com"
 MIRRORS = {
-    BASE_URL: {MIRROR1_URL, MIRROR2_URL},
+    BASE_URL: {MIRROR1_URL, f"{MIRROR2_URL}/"},
 }
 MIRROR_FILES = os.path.join(os.path.dirname(__file__), "mirrors.json")
 
 
-@pytest.fixture
-def cfg():
+@dataclass()
+class FromConfigCase:
+    opts: dict[Key, str]
+    want_error: type[Exception] | None = None
+    want_urls: dict[str, set[str]] | None = None
+
+
+@cases(
+    default=FromConfigCase({}, want_urls={}),
+    mirror_files=FromConfigCase(
+        {"storage.mirrors_files": MIRROR_FILES}, want_urls={f"{BASE_URL}/": {f"{MIRROR1_URL}/", f"{MIRROR2_URL}/"}}
+    ),
+    invalid_mirror_files=FromConfigCase({"storage.mirrors_files": "<!invalid-file-path!>"}, want_error=FileNotFoundError),
+)
+def test_from_config(case: FromConfigCase):
     cfg = Config()
-    cfg.add(Scope.application, "storage", "storage.mirrors_files", MIRROR_FILES)
-    return cfg
+    for k, v in case.opts.items():
+        cfg.add(Scope.application, "storage", k, v)
+
+    try:
+        got_mirrors = MirrorList.from_config(cfg)
+        got_error = None
+    except Exception as ex:
+        got_mirrors = None
+        got_error = ex
+
+    if case.want_urls is not None:
+        assert got_mirrors is not None
+        assert dict(got_mirrors._urls) == case.want_urls  # pylint: disable=protected-access
+    if case.want_error is not None:
+        assert got_error is not None
+        assert isinstance(got_error, case.want_error)
 
 
 @dataclass()
-class FromConfigCase:
+class ResolveCase:
     url: str
     want: set[str]
 
 
 @cases(
-    empty=FromConfigCase("", want=set()),
-    simple=FromConfigCase(URL, want={f"{MIRROR1_URL}/{SOME_PATH}", f"{MIRROR2_URL}/{SOME_PATH}"}),
-    normalized=FromConfigCase("https://rally-tracks.elastic.co/", want={f"{MIRROR1_URL}/", f"{MIRROR2_URL}/"}),
+    empty=ResolveCase("", want=set()),
+    simple=ResolveCase(URL, want={f"{MIRROR1_URL}/{SOME_PATH}", f"{MIRROR2_URL}/{SOME_PATH}"}),
+    normalized=ResolveCase("https://rally-tracks.elastic.co/", want={f"{MIRROR1_URL}/", f"{MIRROR2_URL}/"}),
 )
-def test_from_config(case: FromConfigCase, cfg: Config):
-    mirrors = MirrorList.from_config(cfg)
+def test_resolve(case: ResolveCase):
+    mirrors = MirrorList(urls=MIRRORS)
     got = mirrors.resolve(case.url)
     assert got == case.want
