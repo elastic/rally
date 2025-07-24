d364b13f10f18827ad2594b899afe53bbc3c17bb
Jason Bryan <jbryan@elastic.co>
2025-07-24 16:39:39 -0400

    Fix typo

--- esrally/metrics.py
+++ esrally/metrics.py
@@ -248,10 +248,8 @@ class EsClientFactory:
                     password = self._config.opts("reporting", "datastore.password")
                 except exceptions.ConfigError:
                     raise exceptions.ConfigError(
-                        (
-                            "No password configured through [reporting] configuration or "
-                            "RALLY_REPORTING_DATASTORE_PASSWORD environment variable."
-                        )
+                        "No password configured through [reporting] configuration or "
+                        "RALLY_REPORTING_DATASTORE_PASSWORD environment variable."
                     )
         # Try to get api_key
         try:
