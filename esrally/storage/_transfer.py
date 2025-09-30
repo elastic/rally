4f201164e78ff8a9a7497f7dd68311aeab76a5dd
Federico Ressi <federico.ressi@elastic.co>
2025-09-30 13:38:57 +0200

    Fix some other annotation and one pylint founding.

--- esrally/storage/_transfer.py
+++ esrally/storage/_transfer.py
@@ -22,7 +22,7 @@ import threading
 import time
 from collections.abc import Iterator, Mapping
 from contextlib import contextmanager
-from typing import BinaryIO
+from typing import BinaryIO, Optional
 
 from esrally.storage._adapter import Head, ServiceUnavailableError
 from esrally.storage._client import MAX_CONNECTIONS, Client
@@ -77,7 +77,8 @@ class Transfer:
         url: str,
         path: str,
         executor: Executor,
-        document_length: int | None,
+        *,
+        document_length: int | None = None,
         todo: RangeSet = NO_RANGE,
         done: RangeSet = NO_RANGE,
         multipart_size: int | None = None,
@@ -124,7 +125,7 @@ class Transfer:
         self._multipart_size = multipart_size
         self._todo = todo - done
         self._done = done
-        self._fds: list[FileDescriptor] = []
+        self._fds: list["FileDescriptor"] = []
         self._executor = executor
         self._errors: list[Exception] = []
         self._lock = threading.Lock()
@@ -347,7 +348,7 @@ class Transfer:
                 LOG.warning("error closing file descriptor %r: %s", fd.path, ex)
 
     @contextmanager
-    def _open(self) -> Iterator[FileDescriptor | None]:
+    def _open(self) -> Iterator[Optional["FileDescriptor"]]:
         todo: RangeSet
         with self._lock:
             # It gets a part of the file to transfer from the remaining range.
