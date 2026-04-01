d5854c10f2d994c0cf37fe6e64576d02c17be39a
Federico Ressi <federico.ressi@elastic.co>
2026-04-01 10:41:14 +0200

    Use newer version of ES and improve test case to help implementing more cases.

--- it/docker_dev_image_test.py
+++ it/docker_dev_image_test.py
@@ -14,6 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+import dataclasses
 import logging
 import os
 import subprocess
@@ -26,83 +27,82 @@ from esrally.utils import cases
 
 LOG = logging.getLogger(__name__)
 
+@dataclasses.dataclass
+class ComposeCase:
+    command: str
+    want_return_code: int = 0
+    want_stdout: str | None = None
+    want_stderr: str | None = None
+    compose_file: str = os.path.join(it.ROOT_DIR, "docker", "docker-compose-tests.yml")
+    image_file: str = os.path.join(it.ROOT_DIR, "docker", "dev", "Dockerfile")
+
+    @property
+    def env(self) -> dict[str, str]:
+        return {
+            "TEST_COMMAND": self.command,
+            "RALLY_VERSION": version.__version__,
+            "RALLY_VERSION_TAG": version.__version__,
+            "RALLY_DOCKER_IMAGE": self.image_file,
+        }
+
 
 @cases.cases(
-    arg_name="command",
-    help="--help",
-    race_geonames=(
+    help=ComposeCase("--help"),
+    race=ComposeCase(
         "race --pipeline=benchmark-only --test-mode --track=geonames --challenge=append-no-conflicts-index-only --target-hosts=es01:9200"
     ),
-    override_cmd_race_geonames=(
-        "esrally race --pipeline=benchmark-only --test-mode --track=geonames "
-        "--challenge=append-no-conflicts-index-only --target-hosts=es01:9200"
-    ),
-    list_tracks="list tracks",
 )
-def test_docker_compose(command: str):
-    run_compose(command)
-
-
-def run_compose(
-    command: str,
-    compose_file: str = os.path.join(it.ROOT_DIR, "docker", "docker-compose-tests.yml"),
-    logging_level: int = logging.INFO,
-    check: bool = True,
-    fail: bool = True,
-    tear_down: bool = True,
-) -> subprocess.CompletedProcess:
-    env = {
-        "TEST_COMMAND": command,
-        "RALLY_VERSION": version.__version__,
-        "RALLY_VERSION_TAG": version.__version__,
-    }
-    LOG.log(logging_level, "Running rally with 'docker compose', command='%s', env=%r", command, env)
+def test_docker_compose(case: ComposeCase):
+    LOG.info("Running rally with 'docker compose', command='%s', env=%r", case.command, case.env)
+    env = os.environ.copy()
+    env.update(case.env)
     try:
         result = subprocess.run(
-            f"docker compose -f '{compose_file}' up --abort-on-container-exit",
+            f"docker compose -f '{case.compose_file}' up --abort-on-container-exit",
             env=env,
-            capture_output=False,
+            capture_output=False,  # It defines streams manually
             stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
+            stderr=subprocess.PIPE,
             text=True,
-            check=check,
+            check=case.want_return_code == 0,
             shell=True,
         )
     except subprocess.CalledProcessError as err:
-        msg = (
+        pytest.fail(
             "Docker compose up failed:\n"
             f"  - command: {err.cmd}\n"
             f"  - args: {err.args}\n"
             f"  - return code: {err.returncode}\n"
-            f"  - stderr:\n{err.stderr}"
+            f"  - stdout:\n{err.stdout}\n"
+            f"  - stderr:\n{err.stderr}\n"
         )
-        if fail:
-            pytest.fail(msg)
-        else:
-            LOG.log(logging_level, msg)
-        raise
     else:
-        LOG.log(logging_level, "Docker compose up succeeded. STDOUT:\n%s", result.stdout)
-        return result
+        LOG.debug("Docker compose up succeeded. STDOUT:\n%s", result.stdout)
+        assert result.returncode == case.want_return_code
+        if case.want_stdout is not None:
+            assert result.stdout == case.want_stdout
+        if case.want_stderr is not None:
+            assert result.stderr == case.want_stderr
     finally:
-        if tear_down:
-            try:
-                subprocess.run(
-                    f"docker compose -f '{compose_file}' down -v",
-                    env=env,
-                    capture_output=False,  # It defines streams manually
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.STDOUT,
-                    text=True,
-                    check=True,
-                    shell=True,
-                )
-            except subprocess.CalledProcessError as err:
-                LOG.log(
-                    logging_level,
-                    "Docker compose down failed:\n  - command: '%s'\n  - args: %s\n  - return code: %s\n  - stderr:\n%s",
-                    err.cmd,
-                    err.args,
-                    err.returncode,
-                    err.stderr,
-                )
+        try:
+            LOG.debug("Tear down compose stack...")
+            subprocess.run(
+                f"docker compose -f '{case.compose_file}' down -v",
+                env=case.env,
+                capture_output=False,  # It defines streams manually
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                text=True,
+                check=True,
+                shell=True,
+            )
+        except subprocess.CalledProcessError as err:
+            LOG.error(
+                "Docker compose down failed:\n  - command: '%s'\n  - args: %s\n  - return code: %s\n  - output:\n%s\n",
+                err.cmd,
+                err.args,
+                err.returncode,
+                err.stdout,
+            )
+        else:
+            LOG.debug("Compose stack is down.")
