ec11387a36f2d542a6e4e69a69c29f91ddac353e
Federico Ressi <federico.ressi@elastic.co>
2026-04-01 12:31:10 +0200

    Make sure the es01 container healthcheck works and that test cases of the same module reuses the same instance for it.

--- it/docker_dev_image_test.py
+++ it/docker_dev_image_test.py
@@ -18,6 +18,7 @@ import dataclasses
 import logging
 import os
 import subprocess
+from collections.abc import Generator, Mapping
 
 import pytest
 
@@ -27,23 +28,62 @@ from esrally.utils import cases
 
 LOG = logging.getLogger(__name__)
 
+COMPOSE_FILE = os.path.join(it.ROOT_DIR, "docker", "docker-compose-tests.yml")
+RALLY_DOCKER_IMAGE = os.path.join(it.ROOT_DIR, "docker", "dev", "Dockerfile")
+
+
+def tear_down_stack(compose_env: Mapping[str, str]) -> None:
+    LOG.info("Tearing down docker stack... (compose_env=%s)", compose_env)
+    env = os.environ.copy()
+    env.update(compose_env)
+
+    try:
+        subprocess.run(
+            f"docker compose -f '{COMPOSE_FILE}' down -v",
+            env=env,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True,
+            check=True,
+            shell=True,
+        )
+    except subprocess.CalledProcessError as err:
+        pytest.fail()
+        LOG.error(
+            "Docker compose down failed:\n  - command: '%s'\n  - args: %s\n  - return code: %s\n  - output:\n%s\n",
+            err.cmd,
+            err.args,
+            err.returncode,
+            err.stdout,
+        )
+    else:
+        LOG.debug("Compose stack is down (env=%s).", compose_env)
+
+
+@pytest.fixture(scope="module")
+def compose_env() -> Generator[Mapping[str, str]]:
+    env = {
+        "RALLY_VERSION": version.__version__,
+        "RALLY_VERSION_TAG": version.__version__,
+        "RALLY_DOCKER_IMAGE": RALLY_DOCKER_IMAGE,
+        "COMPOSE_PROJECT_NAME": __name__.replace(".", "_"),
+    }
+    tear_down_stack(env)
+    try:
+        yield env
+    finally:
+        if LOG.isEnabledFor(logging.DEBUG):
+            logs = subprocess.run("docker compose logs -t", capture_output=True, shell=True, check=False, env=env)
+            LOG.debug("Containers logs:\n%s", logs.stdout.decode("utf-8"))
+        tear_down_stack(env)
+
+
 @dataclasses.dataclass
 class ComposeCase:
     command: str
     want_return_code: int = 0
     want_stdout: str | None = None
     want_stderr: str | None = None
-    compose_file: str = os.path.join(it.ROOT_DIR, "docker", "docker-compose-tests.yml")
-    image_file: str = os.path.join(it.ROOT_DIR, "docker", "dev", "Dockerfile")
-
-    @property
-    def env(self) -> dict[str, str]:
-        return {
-            "TEST_COMMAND": self.command,
-            "RALLY_VERSION": version.__version__,
-            "RALLY_VERSION_TAG": version.__version__,
-            "RALLY_DOCKER_IMAGE": self.image_file,
-        }
 
 
 @cases.cases(
@@ -52,57 +92,33 @@ class ComposeCase:
         "race --pipeline=benchmark-only --test-mode --track=geonames --challenge=append-no-conflicts-index-only --target-hosts=es01:9200"
     ),
 )
-def test_docker_compose(case: ComposeCase):
-    LOG.info("Running rally with 'docker compose', command='%s', env=%r", case.command, case.env)
+def test_docker_compose(case: ComposeCase, compose_env: Mapping[str, str]) -> None:
+    LOG.info("Running rally with 'docker compose', command='%s', env=%r", case.command, compose_env)
     env = os.environ.copy()
-    env.update(case.env)
+    env.update(compose_env)
+    env["TEST_COMMAND"] = case.command
     try:
         result = subprocess.run(
-            f"docker compose -f '{case.compose_file}' up --abort-on-container-exit",
+            f"docker compose -f '{COMPOSE_FILE}' run --remove-orphans rally",
             env=env,
-            capture_output=False,  # It defines streams manually
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
+            capture_output=True,
             text=True,
             check=case.want_return_code == 0,
             shell=True,
         )
     except subprocess.CalledProcessError as err:
         pytest.fail(
-            "Docker compose up failed:\n"
+            "Docker compose run failed:\n"
             f"  - command: {err.cmd}\n"
             f"  - args: {err.args}\n"
             f"  - return code: {err.returncode}\n"
             f"  - stdout:\n{err.stdout}\n"
             f"  - stderr:\n{err.stderr}\n"
         )
-    else:
-        LOG.debug("Docker compose up succeeded. STDOUT:\n%s", result.stdout)
-        assert result.returncode == case.want_return_code
-        if case.want_stdout is not None:
-            assert result.stdout == case.want_stdout
-        if case.want_stderr is not None:
-            assert result.stderr == case.want_stderr
-    finally:
-        try:
-            LOG.debug("Tear down compose stack...")
-            subprocess.run(
-                f"docker compose -f '{case.compose_file}' down -v",
-                env=case.env,
-                capture_output=False,  # It defines streams manually
-                stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT,
-                text=True,
-                check=True,
-                shell=True,
-            )
-        except subprocess.CalledProcessError as err:
-            LOG.error(
-                "Docker compose down failed:\n  - command: '%s'\n  - args: %s\n  - return code: %s\n  - output:\n%s\n",
-                err.cmd,
-                err.args,
-                err.returncode,
-                err.stdout,
-            )
-        else:
-            LOG.debug("Compose stack is down.")
+
+    LOG.debug("Docker compose up succeeded. STDOUT:\n%s", result.stdout)
+    assert result.returncode == case.want_return_code
+    if case.want_stdout is not None:
+        assert result.stdout == case.want_stdout
+    if case.want_stderr is not None:
+        assert result.stderr == case.want_stderr
