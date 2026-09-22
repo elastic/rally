02783e915d45ed1e41d23e886f153fd8feb68235
Steve Shulman-Laniel <steve.shulmanlaniel@elastic.co>
2026-09-22 13:09:45 -0400

    `enable_cleanup_closed` only in relevant Python
    The `enable_cleanup_closed` flag is deprecated in newer Python
    `aiohttps`, so we remove it here when appropriate. "When appropriate"
    means "in Python versions >= 3.12.9 or in 3.13.0." This will get rid of
    2,465 warnings.

--- esrally/client/factory.py
+++ esrally/client/factory.py
@@ -17,6 +17,7 @@
 
 import contextlib
 import logging
+import sys
 import time
 from typing import Any
 
@@ -25,6 +26,10 @@ from urllib3.connection import is_ipaddress
 
 from esrally import doc_link, exceptions
 from esrally.utils import console, convert, versions
+
+
+def _needs_cleanup_closed(python_version):
+    return python_version < (3, 12, 8) or (3, 13, 0) <= python_version < (3, 13, 1)
 
 
 class EsClientFactory:
@@ -159,7 +164,8 @@ class EsClientFactory:
         else:
             self.logger.debug("HTTP compression: off")
 
-        self.enable_cleanup_closed = convert.to_bool(self.client_options.pop("enable_cleanup_closed", True))
+        enable_cleanup_closed = convert.to_bool(self.client_options.pop("enable_cleanup_closed", True))
+        self.enable_cleanup_closed = enable_cleanup_closed and _needs_cleanup_closed(sys.version_info)
         self.max_connections = max(256, self.client_options.pop("max_connections", 0))
         self.static_responses = self.client_options.pop("static_responses", None)
 
